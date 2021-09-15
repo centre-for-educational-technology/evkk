72100dd93a29e368f53cef67092e84623d2550a5
mikktarvas <mikk.tarvas@gmail.com>
2021-09-15 18:12:37 +0300

    Build script.

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -e
 
-BUILD_TARGET=demo
+BUILD_TARGET=demo #TODO:
 
 # Remove build dir (if exists)
-#rm -rf ./build/
+rm -rf ./build/
 
 # Build docker images
-#docker build . -f ./docker/Dockerfile.backend -t evkk-backend --no-cache
-#docker build . -f ./docker/Dockerfile.ui -t evkk-ui --no-cache
+docker build . -f ./docker/Dockerfile.backend -t evkk-backend --no-cache
+docker build . -f ./docker/Dockerfile.ui -t evkk-ui --no-cache
 
 # Copy compose files
 mkdir -p ./build/compose/
 cp -r ./docker/compose/ ./build/compose/
 
 # Save docker images
-#mkdir -p ./build/images/
-#docker save -o ./build/images/evkk-backend.tar evkk-backend
-#docker save -o ./build/images/evkk-ui.tar evkk-ui
+mkdir -p ./build/images/
+docker save -o ./build/images/evkk-backend.tar evkk-backend
+docker save -o ./build/images/evkk-ui.tar evkk-ui
 
 # Copy env files and remove `${BUILD_TARGET}_` prefix
 mkdir -p ./build/conf/
