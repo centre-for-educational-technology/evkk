beceff84021071c7807b335ce79d583f849a1778
mikktarvas <mikk.tarvas@gmail.com>
2021-09-15 12:26:20 +0300

    Run build script.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Remove build dir (if exists)
+rm -rf ./build/
+
+# Build docker images
+docker build . -f ./docker/Dockerfile.backend -t evkk-backend --no-cache
+docker build . -f ./docker/Dockerfile.ui -t evkk-ui --no-cache
+
+# Copy compose files
+mkdir -p ./build/compose/
+cp -r ./docker/compose/ ./build/compose/
+
+# Save docker images
+mkdir -p ./build/images/
+docker save -o ./build/images/evkk-backend.tar evkk-backend
+docker save -o ./build/images/evkk-ui.tar evkk-ui
