ccd621406894a9ea7d278868af137bf6cb664746
mikktarvas <mikk.tarvas@gmail.com>
2021-09-21 10:08:25 +0300

    Build script.

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Running EVKK ..."
 for file in ./images/*.tar; do docker load -i ${file}; done
-docker-compose -f compose/docker-compose.base.yml up
+docker-compose -f compose/docker-compose.base.yml -f compose/docker-compose.yml up
