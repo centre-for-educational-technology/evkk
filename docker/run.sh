303c9da4307886960a1b6921d9e2526741373801
mikktarvas <mikk.tarvas@gmail.com>
2021-09-25 15:36:34 +0300

    Prod conf.

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,5 @@ set -e
 
 echo "Running EVKK ..."
 for file in ./images/*.tar; do docker load -i ${file}; done
+docker-compose -f compose/docker-compose.base.yml -f compose/docker-compose.yml down
 docker-compose -f compose/docker-compose.base.yml -f compose/docker-compose.yml up
