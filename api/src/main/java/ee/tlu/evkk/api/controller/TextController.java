a0afc17f0cd9e2913acb540f98c22417fc46489a
mikktarvas <mikk.tarvas@gmail.com>
2021-09-30 21:48:54 +0300

    Format TextController.

--- api/src/main/java/ee/tlu/evkk/api/controller/TextController.java
+++ api/src/main/java/ee/tlu/evkk/api/controller/TextController.java
@@ -1,111 +1,125 @@
 package ee.tlu.evkk.api.controller;
-import org.springframework.web.bind.annotation.*;
+
 import ee.tlu.evkk.api.dao.TextDao;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
-import java.lang.ProcessBuilder;
-import java.util.*;
 import java.io.*;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/texts")
 public class TextController {
-  TextDao textDao;
+
+  private final TextDao textDao;
+
   public TextController(TextDao uusTDao) {
     textDao = uusTDao;
   }
+
   @GetMapping("/kysitekst")
   public String kysiTekst(String id) {
-      return textDao.findTextById(UUID.fromString(id));
+    return textDao.findTextById(UUID.fromString(id));
   }
+
   @GetMapping("/kysikorpusetekstiIDd")
   public List<String> kysikorpusetekstiIDd(String korpusekood) {
-      return textDao.findTextIDsByCorpusID(korpusekood);
+    return textDao.findTextIDsByCorpusID(korpusekood);
   }
+
   @GetMapping("/kysikorpusetekstiIDjapealkiri")
   public List<String> kysikorpusetekstiIDjapealkiri(String korpusekood) {
-      return textDao.findTextIDandTitleByCorpusID(korpusekood);
+    return textDao.findTextIDandTitleByCorpusID(korpusekood);
   }
+
   @GetMapping("/tervitus")
-  public String tervitus(){
-      return "Tere";
+  public String tervitus() {
+    return "Tere";
   }
+
   @GetMapping("/nimetervitus")
   public String nimetervitus(String eesnimi) {
-      return "Tere, " + eesnimi;
+    return "Tere, " + eesnimi;
   }
+
   @PostMapping("/lemmad")
   public String lemmad(String tekst) throws Exception {
-      ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/lemmad.py");
-      processBuilder.redirectErrorStream(true);
-      String tulemus = "";
-    
-      Process process = processBuilder.start();
-      OutputStream stdin = process.getOutputStream();
-      BufferedReader lugeja = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-      writer.write(tekst + "\n");
-      writer.close();
-      for(int i = 0; i < 14; i++) {
-          String line = lugeja.readLine();
-      }
+    ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/lemmad.py");
+    processBuilder.redirectErrorStream(true);
+    String tulemus = "";
+
+    Process process = processBuilder.start();
+    OutputStream stdin = process.getOutputStream();
+    BufferedReader lugeja = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+    writer.write(tekst + "\n");
+    writer.close();
+    for (int i = 0; i < 14; i++) {
       String line = lugeja.readLine();
-      while(line != null) {
-          //System.out.println(line);
-          tulemus += line;
-          line = lugeja.readLine();
-      }
-      return tulemus;
     }
+    String line = lugeja.readLine();
+    while (line != null) {
+      //System.out.println(line);
+      tulemus += line;
+      line = lugeja.readLine();
+    }
+    return tulemus;
+  }
+
   @PostMapping("/sonad")
   public String sonad(String tekst) throws Exception {
-      ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/sonad.py");
-      processBuilder.redirectErrorStream(true);
-      String tulemus = "";
-    
-      Process process = processBuilder.start();
-      OutputStream stdin = process.getOutputStream();
-      BufferedReader lugeja = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-      writer.write(tekst + "\n");
-      writer.close();
-      for(int i = 0; i < 12; i++) {
-          String line = lugeja.readLine();
-          System.out.println(line);
-      }
+    ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/sonad.py");
+    processBuilder.redirectErrorStream(true);
+    String tulemus = "";
+
+    Process process = processBuilder.start();
+    OutputStream stdin = process.getOutputStream();
+    BufferedReader lugeja = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+    writer.write(tekst + "\n");
+    writer.close();
+    for (int i = 0; i < 12; i++) {
       String line = lugeja.readLine();
-      while(line != null) {
-          tulemus += line;
-          line = lugeja.readLine();
-      }
-      System.out.println(tulemus);
-      return tulemus;
+      System.out.println(line);
     }
-    @PostMapping("/laused")
+    String line = lugeja.readLine();
+    while (line != null) {
+      tulemus += line;
+      line = lugeja.readLine();
+    }
+    System.out.println(tulemus);
+    return tulemus;
+  }
+
+  @PostMapping("/laused")
   public String laused(String tekst) throws Exception {
-      ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/laused.py");
-      processBuilder.redirectErrorStream(true);
-      String tulemus = "";
-    
-      Process process = processBuilder.start();
-      OutputStream stdin = process.getOutputStream();
-      BufferedReader lugeja = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-      writer.write(tekst + "\n");
-      writer.close();
-      for(int i = 0; i < 10; i++) {
-          String line = lugeja.readLine();
-      }
+    ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/laused.py");
+    processBuilder.redirectErrorStream(true);
+    String tulemus = "";
+
+    Process process = processBuilder.start();
+    OutputStream stdin = process.getOutputStream();
+    BufferedReader lugeja = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+    writer.write(tekst + "\n");
+    writer.close();
+    for (int i = 0; i < 10; i++) {
       String line = lugeja.readLine();
-      while(line != null) {
-          tulemus += line;
-          line = lugeja.readLine();
-      }
-      return tulemus;
     }
-    @GetMapping("/asukoht")
-    public String asukoht(){
-       return new java.io.File(".").getAbsolutePath().toString();
+    String line = lugeja.readLine();
+    while (line != null) {
+      tulemus += line;
+      line = lugeja.readLine();
     }
+    return tulemus;
+  }
+
+  @GetMapping("/asukoht")
+  public String asukoht() {
+    return new java.io.File(".").getAbsolutePath().toString();
+  }
+
 }
