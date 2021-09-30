c89e3ab5801c5f7ab1cc6f0b550b1f9c391fac56
mikktarvas <mikk.tarvas@gmail.com>
2021-10-01 00:55:19 +0300

    lemmad

--- sonarakendus/sonarakendus/javascript/sonasagedus.js
+++ sonarakendus/sonarakendus/javascript/sonasagedus.js
@@ -25,7 +25,7 @@ freeze = true;
 if(vorm == "algvormid") {
     $.ajax({
         type: "POST",
-        url: "https://evkk.tlu.ee/sonarakendus/api/texts/lemmad",
+        url: "https://evkk.tlu.ee/api/texts/lemmad",
         contentType:"application/json; charset=utf-8",
         data: JSON.stringify( {tekst : koguTekst[0]}),
         success: function(data){
