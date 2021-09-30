0f9ff2f9ea74b52bee7c6bda83967dd404e95783
Harli Kodasma <harlikodasma@gmail.com>
2021-10-01 00:46:04 +0300

    korpuspäring

--- api/src/main/java/ee/tlu/evkk/api/controller/TextController.java
+++ api/src/main/java/ee/tlu/evkk/api/controller/TextController.java
@@ -1,4 +1,5 @@
 package ee.tlu.evkk.api.controller;
+import org.json.*;
 import org.springframework.web.bind.annotation.*;
 import ee.tlu.evkk.api.dao.TextDao;
 
@@ -27,6 +28,14 @@ public class TextController {
   public List<String> kysikorpusetekstiIDjapealkiri(String korpusekood) {
       return textDao.findTextIDandTitleByCorpusID(korpusekood);
   }
+//   @GetMapping("/detailneparing")
+//   public List<String> detailneparing(String queryJoin, String queryWhere) {
+//       return textDao.textTitleQueryByParameters(queryJoin, queryWhere);
+//   }
+//   @GetMapping("/detailneparing")
+//   public List<String> detailneparing(String queryJoin) {
+//       return textDao.textTitleQueryByParameters(queryJoin);
+//   }
   @GetMapping("/tervitus")
   public String tervitus(){
       return "Tere";
@@ -37,7 +46,7 @@ public class TextController {
   }
   @PostMapping("/lemmad")
   public String lemmad(String tekst) throws Exception {
-      ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/lemmad.py");
+      ProcessBuilder processBuilder = new ProcessBuilder("/opt/miniconda3/bin/python", "../ui/public/python/lemmad.py");
       processBuilder.redirectErrorStream(true);
       String tulemus = "";
     
@@ -60,7 +69,7 @@ public class TextController {
     }
   @PostMapping("/sonad")
   public String sonad(String tekst) throws Exception {
-      ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/sonad.py");
+      ProcessBuilder processBuilder = new ProcessBuilder("/opt/miniconda3/bin/python", "../ui/public/python/sonad.py");
       processBuilder.redirectErrorStream(true);
       String tulemus = "";
     
@@ -84,7 +93,7 @@ public class TextController {
     }
     @PostMapping("/laused")
   public String laused(String tekst) throws Exception {
-      ProcessBuilder processBuilder = new ProcessBuilder("python", "../ui/public/python/laused.py");
+      ProcessBuilder processBuilder = new ProcessBuilder("/opt/miniconda3/bin/python", "../ui/public/python/laused.py");
       processBuilder.redirectErrorStream(true);
       String tulemus = "";
     
@@ -108,4 +117,123 @@ public class TextController {
     public String asukoht(){
        return new java.io.File(".").getAbsolutePath().toString();
     }
+
+ @CrossOrigin("*")
+    @PostMapping("/detailneparing")
+    public List<String> detailneparing(String queryJoin, String queryWhere) {
+        return textDao.textTitleQueryByParameters(queryJoin, queryWhere);
+    }
+    @GetMapping("/getValues")
+    public String getValues(String cId) {
+        return textDao.findValueByPropertyName(cId);
+    }
+
+    @GetMapping("/getMiniStats")
+    public String getMiniStats(String corpus) {
+        String[] corpusArray = corpus.split(",");
+        return textDao.findMiniStats(corpusArray);
+    }
+
+    @GetMapping("/getDetailedValues")
+    public String getValues(@RequestParam("corpus") String corpus, String pValue, String pName) {
+        String[] corpusArray = corpus.split(",");
+        String[] pValueArray = pValue.split(",");
+        return textDao.findDetailedValueByPropertyName(pValueArray, pName, corpusArray);
+    }
+
+    @GetMapping("/detailedSearch")
+    public String getDetailedSearch(@RequestParam("data") String data) {
+        System.out.println(data);
+        try {
+        JSONObject json = new JSONObject(data);
+
+        String pName = json.getString("pName"); // pName - main filter (property_name)
+        
+        ArrayList<String> pValue = new ArrayList<String>(); // pValue - array of selected filters (property_value)
+        JSONArray arr = json.getJSONArray("pValue");
+        if (arr != null) { 
+           for (int i=0; i < arr.length(); i++){ 
+            pValue.add(arr.getString(i));
+           } 
+        } 
+
+        ArrayList<String> corpus = new ArrayList<String>(); // corpus - array of selected corpuses
+        arr = json.getJSONArray("corpus");
+        if (arr != null) { 
+            for (int i=0; i < arr.length(); i++){ 
+                corpus.add(arr.getString(i));
+            } 
+        }
+
+        ArrayList<String> selectedFilters = new ArrayList<String>(); // selectedFilters - array of selected filters
+        // [tekstikeel, vanus] - selectedFilters
+        ArrayList<ArrayList<String>> filterValues = new ArrayList<ArrayList<String>>(); // filterValues - array of selected filters
+        // [[eesti, vene, inglise],[kuni18,kuni26,41plus]] - filterValues
+
+        arr = json.getJSONArray("selectedFilters");
+        if (arr != null) { 
+            for (int i=0; i < arr.length(); i++){ 
+                selectedFilters.add(arr.getJSONObject(i).getString("filter"));
+                ArrayList<String> arrValues = new ArrayList<String>();
+                // filterValues.add(arr.getJSONObject(i).getJSONArray("data"));
+                for (int y = 0; y < arr.getJSONObject(i).getJSONArray("data").length(); y++) {
+                    String str = arr.getJSONObject(i).getJSONArray("data").getString(y);
+                    if (str == "tundmatu" || str.equals("tundmatu")) {
+                        arrValues.add("''");
+                    } else {
+                        arrValues.add("'"+ str + "'");
+                    }
+
+                }
+                filterValues.add(arrValues);
+            } 
+        }
+
+        // converting arrayList to String[] array
+        String[] pValueArray = new String[pValue.size()];
+        pValueArray = pValue.toArray(pValueArray);
+
+        String[] corpusArray = new String[corpus.size()];
+        corpusArray = corpus.toArray(corpusArray);
+
+        // constructing SQL queries
+        String join = "";
+        String condition = "";
+        for (int i = 0; i < selectedFilters.size(); i++) {
+            join+= "JOIN core.text_property as tp" + (i + 6) + " on tp" + (i + 5) + ".text_id=tp" + (i + 6) + ".text_id\n";
+            String values = String.join(",", filterValues.get(i));
+            condition+= "AND tp" + (i + 6) + ".property_name = '" + selectedFilters.get(i) +"' AND tp" + (i + 6) + ".property_value IN ("+ values +")\n";
+        }
+
+
+        return textDao.detailedSearch(pValueArray, pName, corpusArray, join, condition);
+        // return pValueArray;
+
+
+        } catch (JSONException err){
+            System.out.println("Error" + err.toString());
+            JSONObject json = new JSONObject(data);
+            System.out.println("TEST: " + json.getJSONArray("pValue"));
+            // String[] er = {"oops","ei toota"};
+            // return er;
+            return "oops, ei tööta";
+
+       }
+
+    //    System.out.println(JsonObject.getJSONObject("selectedFilters"));
+        // return textDao.detailedSearch(pValueArray, pName, corpusArray, filterNamesArray, filterValuesArray);
+    }
+
+    @GetMapping("/getAvailableValues")
+    public String getAvailableValues(String pName) {
+        return textDao.findAvailableValues(pName);
+    }
+
+
+
+    @GetMapping("/kysikorpusetekstid")
+    public List<String> kysiKorpuseTekstid(String id){
+        return textDao.findTextsByCorpusId(id);
+    }
+
 }
---
+++ sonarakendus/javascript/paring.js
@@ -0,0 +1,231 @@
+document.querySelector("#texts").style.display = "none";
+
+tekstideIDd = [];
+tekstidePealkirjad = [];
+
+document.getElementById("type").addEventListener("click", function() {
+    if(document.getElementById("type").value != "teadmata") {
+        document.getElementById("typeOn").checked = true;
+    } else {
+        document.getElementById("typeOn").checked = false;
+    }
+});
+
+document.getElementById("level").addEventListener("click", function() {
+    if(document.getElementById("level").value != "teadmata") {
+        document.getElementById("levelOn").checked = true;
+    } else {
+        document.getElementById("levelOn").checked = false;
+    }
+});
+
+document.getElementById("characters").addEventListener("change", function() {
+    if(document.getElementById("characters").value != "") {
+        document.getElementById("charactersOn").checked = true;
+    } else {
+        document.getElementById("charactersOn").checked = false;
+    }
+});
+
+document.getElementById("nativelang").addEventListener("click", function() {
+    if(document.getElementById("nativelang").value != "teadmata") {
+        document.getElementById("nativelangOn").checked = true;
+    } else {
+        document.getElementById("nativelangOn").checked = false;
+    }
+});
+
+document.getElementById("homelang").addEventListener("click", function() {
+    if(document.getElementById("homelang").value != "teadmata") {
+        document.getElementById("homelangOn").checked = true;
+    } else {
+        document.getElementById("homelangOn").checked = false;
+    }
+});
+
+document.getElementById("gender").addEventListener("click", function() {
+    if(document.getElementById("gender").value != "teadmata") {
+        document.getElementById("genderOn").checked = true;
+    } else {
+        document.getElementById("genderOn").checked = false;
+    }
+});
+
+document.getElementById("age").addEventListener("click", function() {
+    if(document.getElementById("age").value != "teadmata") {
+        document.getElementById("ageOn").checked = true;
+    } else {
+        document.getElementById("ageOn").checked = false;
+    }
+});
+
+document.getElementById("country").addEventListener("click", function() {
+    if(document.getElementById("country").value != "teadmata") {
+        document.getElementById("countryOn").checked = true;
+    } else {
+        document.getElementById("countryOn").checked = false;
+    }
+});
+
+function submitted() {
+    parameters = ["korpus", "tekstityyp", "keeletase", "characters", "emakeel", "kodukeel", "sugu", "vanus", "elukoht"]; //exercise
+    values = [];
+    if(document.querySelector("#korpus").value != "koik") {
+        corpus = document.querySelector("#korpus").value;
+        values.push(corpus);
+    } else {
+        corpus = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#typeOn").checked) {
+        type = document.querySelector("#type").value;
+        values.push(type);
+    } else {
+        type = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#levelOn").checked) {
+        level = document.querySelector("#level").value;
+        values.push(level);
+    } else {
+        level = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#charactersOn").checked) {
+        characters = document.querySelector("#characters").value;
+        values.push(characters);
+    } else {
+        characters = "NO";
+        values.push("NO");
+    }
+    // if(document.querySelector("#exercise").checked) {
+    //     exercise = true;
+    //     values.push(true);
+    // } else {
+    //     exercise = "NO";
+    //     values.push("NO");
+    // }
+    if(document.querySelector("#nativelangOn").checked) {
+        nativelang = document.querySelector("#nativelang").value;
+        values.push(nativelang);
+    } else {
+        nativelang = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#homelangOn").checked) {
+        homelang = document.querySelector("#homelang").value;
+        values.push(homelang);
+    } else {
+        homelang = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#genderOn").checked) {
+        gender = document.querySelector("#gender").value;
+        values.push(gender);
+    } else {
+        gender = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#ageOn").checked) {
+        age = document.querySelector("#age").value;
+        values.push(age);
+    } else {
+        age = "NO";
+        values.push("NO");
+    }
+    if(document.querySelector("#countryOn").checked) {
+        country = document.querySelector("#country").value;
+        if(country == "eesti") {
+            values.push(["idaviru", "tallinn", "tartu"]);
+        } else {
+            values.push(country);
+        }
+    } else {
+        country = "NO";
+        values.push("NO");
+    }
+
+    queryJoinPart = "";
+    queryWherePart = "";
+    startingNumber = 3;
+    for(let i = 0; i < parameters.length; i++) {
+        if(Array.isArray(values[i])) {
+            queryJoinPart += "join core.text_property as p" + startingNumber + " on p2.text_id = p" + startingNumber + ".text_id ";
+            queryWherePart += " and p" + startingNumber + ".property_name = '" + parameters[i] + "' and p" + startingNumber + ".property_value in ("
+            for(let j = 0; j < values[i].length; j++) {
+                queryWherePart += "'" + values[i][j] + "', ";
+            }
+            queryWherePart = queryWherePart.slice(0, -2);
+            queryWherePart += ")";
+            startingNumber++;
+        } else if(parameters[i] == "characters" && values[i] != "NO") {
+            queryJoinPart += "join core.text as p" + startingNumber + " on p2.text_id = p" + startingNumber + ".id ";
+            queryWherePart += " and char_length(p" + startingNumber + ".content) <= " + values[i];
+            startingNumber++;
+        } else if(values[i] != "NO") {
+            queryJoinPart += "join core.text_property as p" + startingNumber + " on p2.text_id = p" + startingNumber + ".text_id ";
+            queryWherePart += " and p" + startingNumber + ".property_name = '" + parameters[i] + "' and p" + startingNumber + ".property_value = '" + values[i] + "'";
+            startingNumber++;
+        }
+    }
+    queryWherePart += ";";
+    //console.log(queryJoinPart);
+    //console.log(queryWherePart);
+
+    $.ajax({
+        type: "POST",
+        url: "api/texts/detailneparing",
+        data: {queryJoin : queryJoinPart,
+        queryWhere : queryWherePart},
+        success: function(data) {
+            for(let i = 0; i < data.length; i++) {
+                tekstideIDd.push(data[i].split("!!!")[0]);
+                if(data[i].split("!!!")[1] == "") {
+                    tekstidePealkirjad.push("pealkiri puudub");
+                } else {
+                    tekstidePealkirjad.push(data[i].split("!!!")[1]);
+                }
+            }
+            tekstideKuvamine();
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert(XMLHttpRequest + "\n" + textStatus + "\n" + errorThrown);
+        }
+    });
+
+    tekstideKuvamine();
+}
+
+function tekstideKuvamine() {
+        let puhver = [];
+        for(let i = 0; i < tekstideIDd.length; i++) {
+            puhver.push("<tr value='" + tekstideIDd[i] + "'><a href='javascript:eelvaade(\"" + tekstideIDd[i] + "\")'>" + tekstidePealkirjad[i] + "</a></tr>");
+        }
+        kaivitaTabel(puhver);
+    }
+
+    function eelvaade(tekstiID) {
+        $.ajax({
+            type: "GET",
+            url: "api/texts/kysitekst",
+            data: {id : tekstiID},
+            success: function(data){
+                tekstisisu = data.split("!!!")[1];
+                pealkirjaID = tekstideIDd.indexOf(tekstiID);
+                pealkiri = tekstidePealkirjad[pealkirjaID];
+                localStorage.setItem("kuvatavtekst", tekstisisu);
+                localStorage.setItem("tekstipealkiri", pealkiri);
+                window.open("tekst.html", "_blank");
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                alert(textStatus + "\n" + errorThrown);
+            }
+        });
+    }
+
+    function kaivitaTabel(puhver) {
+        document.querySelector("#tabel").innerHTML = puhver.join(' ');
+        document.querySelector("#vorm").style.display = "none";
+        document.querySelector("#texts").style.display = "block";
+        document.querySelector("#textAmount").innerHTML = "Leiti " + puhver.length + " teksti.";
+    }
