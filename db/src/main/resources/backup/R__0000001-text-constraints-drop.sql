13488c2c05286726633a55c08f4fb055ce957144
mikktarvas <mikk.tarvas@gmail.com>
2021-10-01 18:54:14 +0300

    text data

---
+++ db/src/main/resources/backup/R__0000001-text-constraints-drop.sql
@@ -0,0 +1,3 @@
+alter table core.text_property drop constraint text_property_text_id_fkey;
+alter table core.text_property drop constraint text_property_pkey;
+alter table core.text drop constraint text_pkey;
