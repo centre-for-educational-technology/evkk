0e7c611e42c022606f2e932de469502b53f9f24f
mikktarvas <mikk.tarvas@gmail.com>
2021-10-15 00:09:10 +0300

    db setup

--- db/src/main/resources/migration/callbacks/beforeMigrate.sql
+++ db/src/main/resources/migration/callbacks/beforeMigrate.sql
@@ -30,23 +30,38 @@ begin
 end;
 $$ language plpgsql;
 
+create procedure pg_temp.create_schema(name text) as
+$$
+begin
+
+  execute format('create schema if not exists %s', name);
+  execute format('grant usage on schema %s to public', name);
+
+  execute format('call pg_temp.create_role(''%s_rw'')', name);
+  execute format('call pg_temp.create_role(''%s_r'')', name);
+
+end;
+$$ language plpgsql;
+
 ------------------
 -- CREATE USERS --
 ------------------
 
 call pg_temp.create_user('api_user', '${EVKK_API_DATASOURCE_PASSWORD}');
 call pg_temp.create_user('backup_user', '${EVKK_BACKUP_DATASOURCE_PASSWORD}');
---todo: call pg_temp.create_user('daemon_user', '{EVKK_DAEMON_DATASOURCE_PASSWORD}');
 
 ------------------
 -- CREATE ROLES --
 ------------------
 
-call pg_temp.create_role('sys_rw');
-call pg_temp.create_role('sys_r');
+call pg_temp.create_schema('sys');
+call pg_temp.create_schema('core');
 
-call pg_temp.create_role('core_rw');
-call pg_temp.create_role('core_r');
+--call pg_temp.create_role('sys_rw');
+--call pg_temp.create_role('sys_r');
+
+--call pg_temp.create_role('core_rw');
+--call pg_temp.create_role('core_r');
 
 -----------------
 -- GRANT ROLES --
@@ -63,11 +78,11 @@ grant core_rw to api_user;
 
 drop schema if exists public cascade;
 
-create schema if not exists sys;
-grant usage on schema sys to public;
+--create schema if not exists sys;
+--grant usage on schema sys to public;
 
-create schema if not exists core;
-grant usage on schema core to public;
+--create schema if not exists core;
+--grant usage on schema core to public;
 
 ----------------
 -- PRIVILEGES --
@@ -75,11 +90,14 @@ grant usage on schema core to public;
 
 ---
 
-grant select on all tables in schema sys to sys_r;
 
+grant select, insert, update on all tables in schema sys to sys_rw;
+grant usage on all sequences in schema sys to sys_rw;
 alter default privileges in schema sys grant select, insert, update on tables to group sys_rw;
 alter default privileges in schema sys grant usage on sequences to group sys_rw;
 
+grant select on all tables in schema sys to sys_r;
+grant select on all sequences in schema sys to sys_r;
 alter default privileges in schema sys grant select on tables to group sys_r;
 alter default privileges in schema sys grant select on sequences to group sys_r;
 
