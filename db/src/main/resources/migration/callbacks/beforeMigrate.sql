f558d4cbba69df2221ce1fea6d96ca2cd9e0f902
mikktarvas <mikk.tarvas@gmail.com>
2021-10-15 00:21:16 +0300

    db setup

--- db/src/main/resources/migration/callbacks/beforeMigrate.sql
+++ db/src/main/resources/migration/callbacks/beforeMigrate.sql
@@ -38,10 +38,28 @@ begin
   execute format('grant usage on schema %s to public', name);
 
   execute format('call pg_temp.create_role(''%s_rw'')', name);
+  execute format('grant select, insert, update on all tables in schema %1$s to %1$s_rw', name);
+  execute format('grant usage on all sequences in schema %1$s to %1$s_rw', name);
+  execute format('alter default privileges in schema %1$s grant select, insert, update on tables to group %1$s_rw', name);
+  execute format('alter default privileges in schema %1$s grant usage on sequences to group %1$s_rw', name);
+
   execute format('call pg_temp.create_role(''%s_r'')', name);
+  execute format('grant select on all tables in schema %1$s to %1$s_r', name);
+  execute format('grant select on all sequences in schema %1$s to %1$s_r', name);
+  execute format('alter default privileges in schema %1$s grant select on tables to group %1$s_r', name);
+  execute format('alter default privileges in schema %1$s grant select on sequences to group %1$s_r', name);
 
 end;
 $$ language plpgsql;
+
+-----------------------
+-- CONFIGURE SCHEMAS --
+-----------------------
+
+drop schema if exists public cascade;
+
+call pg_temp.create_schema('sys');
+call pg_temp.create_schema('core');
 
 ------------------
 -- CREATE USERS --
@@ -49,19 +67,6 @@ $$ language plpgsql;
 
 call pg_temp.create_user('api_user', '${EVKK_API_DATASOURCE_PASSWORD}');
 call pg_temp.create_user('backup_user', '${EVKK_BACKUP_DATASOURCE_PASSWORD}');
-
-------------------
--- CREATE ROLES --
-------------------
-
-call pg_temp.create_schema('sys');
-call pg_temp.create_schema('core');
-
---call pg_temp.create_role('sys_rw');
---call pg_temp.create_role('sys_r');
-
---call pg_temp.create_role('core_rw');
---call pg_temp.create_role('core_r');
 
 -----------------
 -- GRANT ROLES --
@@ -71,42 +76,6 @@ grant sys_r to backup_user;
 grant core_r to backup_user;
 
 grant core_rw to api_user;
-
------------------------
--- CONFIGURE SCHEMAS --
------------------------
-
-drop schema if exists public cascade;
-
---create schema if not exists sys;
---grant usage on schema sys to public;
-
---create schema if not exists core;
---grant usage on schema core to public;
-
-----------------
--- PRIVILEGES --
-----------------
-
----
-
-
-grant select, insert, update on all tables in schema sys to sys_rw;
-grant usage on all sequences in schema sys to sys_rw;
-alter default privileges in schema sys grant select, insert, update on tables to group sys_rw;
-alter default privileges in schema sys grant usage on sequences to group sys_rw;
-
-grant select on all tables in schema sys to sys_r;
-grant select on all sequences in schema sys to sys_r;
-alter default privileges in schema sys grant select on tables to group sys_r;
-alter default privileges in schema sys grant select on sequences to group sys_r;
-
----
-
-alter default privileges in schema core grant select, insert, update on tables to group core_rw;
-alter default privileges in schema core grant usage on sequences to group core_rw;
-alter default privileges in schema core grant select on tables to group core_r;
-alter default privileges in schema core grant select on sequences to group core_r;
 
 ----------------
 -- EXTENSIONS --
