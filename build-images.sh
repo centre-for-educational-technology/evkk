9e2062a122d3edc727583a3ce7418f04100e9870
mikktarvas <mikk.tarvas@gmail.com>
2021-11-04 19:41:10 +0200

    build.sh

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker build . -f ./images/evkk-stanza.Dockerfile -t evkk-stanza --no-cache
-docker build . -f ./images/evkk-estnltk141.Dockerfile -t evkk-estnltk141 --no-cache
-docker build . -f ./images/evkk-jdk8-mvn.Dockerfile -t evkk-jdk8-mvn --no-cache
+docker build . -f ./images/evkk-stanza.Dockerfile -t evkk-stanza
+docker build . -f ./images/evkk-estnltk141.Dockerfile -t evkk-estnltk141
+docker build . -f ./images/evkk-jdk8-mvn.Dockerfile -t evkk-jdk8-mvn
