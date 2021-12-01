8dbc6f6460243faea31a94e7bcbdc8ad1eef3c25
Harli Kodasma <harlikodasma@gmail.com>
2021-12-01 20:17:57 +0200

    uued failid tõstetud ka serveri kausta

--- sonarakendus/sonarakendus/javascript/tekst.js
+++ sonarakendus/sonarakendus/javascript/tekst.js
@@ -2,7 +2,7 @@ let pealkiri = document.querySelector("#pealkiri");
 let sisu = document.querySelector("#sisu");
 
 pealkiri.innerHTML = localStorage.getItem("tekstipealkiri");
-sisu.innerHTML = localStorage.getItem("kuvatavtekst");
+sisu.innerHTML = localStorage.getItem("kuvatavtekst").replace(/\\n/g, '<br>');
 
 function sulge() {
     window.close();
