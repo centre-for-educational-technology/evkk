3749204394e70c62a3d6ab4ae22e6d79a032d345
Harli Kodasma <harlikodasma@gmail.com>
2021-12-02 18:29:21 +0200

    Added new menu item

---
+++ ui/src/views/Harli.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Harli extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello World Harli!
+            </div>
+        );
+    }
+
+}
+
+export default Harli;
