f321ccac1b3260e4e210376642ad0ead6cf75428
mikktarvas <mikk.tarvas@gmail.com>
2022-01-20 21:00:44 +0200

    Added hash to text.

---
+++ db/src/main/resources/migration/versioned/V0003__Migration.sql
@@ -0,0 +1,17 @@
+create function core.text_hash(content text) returns text as
+$$
+begin
+  return encode(sha512(convert_to(content::text, 'UTF-8')), 'hex');
+end;
+$$ language plpgsql immutable;
+
+alter table core.text
+  add column hash text generated always as ( core.text_hash(content) ) stored;
+
+update core.text
+set hash = default;
+
+alter table core.text
+  alter column hash set not null;
+
+comment on column core.text.hash is 'Computed hash for text content';
