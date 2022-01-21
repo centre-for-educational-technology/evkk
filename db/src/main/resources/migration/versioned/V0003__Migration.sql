e59a6b3dce4d768ca2b5315c19d067c6fb150ab4
mikktarvas <mikk.tarvas@gmail.com>
2022-01-21 03:35:38 +0200

    core.text_processor_result

--- db/src/main/resources/migration/versioned/V0003__Migration.sql
+++ db/src/main/resources/migration/versioned/V0003__Migration.sql
@@ -15,3 +15,26 @@ alter table core.text
   alter column hash set not null;
 
 comment on column core.text.hash is 'Computed hash for text content';
+
+create index text_idx_hash on core.text (hash);
+
+create table core.text_processor_result
+(
+  text_processor_result_id uuid default uuid_generate_v4(),
+  text_hash                text   not null,
+  type                     text   not null,
+  version                  bigint not null,
+  result                   jsonb  not null,
+
+  constraint text_processor_result_uq_text_hash_type_version unique (text_hash, type, version)
+);
+
+comment on table core.text_processor_result is 'Results for text processors';
+comment on column core.text_processor_result.text_processor_result_id is 'Primary key';
+comment on column core.text_processor_result.text_hash is 'Hash code computed for the text in core.text.hash';
+comment on column core.text_processor_result.type is 'Processor type';
+comment on column core.text_processor_result.version is 'Processor version';
+comment on column core.text_processor_result.result is 'Processor result';
+
+call core.attach_meta_trigger('core.text_processor_result');
+create index text_processor_result_idx_text_hash on core.text_processor_result (text_hash);
