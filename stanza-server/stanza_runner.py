1832c32cda5999b42011f277072a30b484756889
mikktarvas <mikk.tarvas@gmail.com>
2022-01-25 00:41:00 +0200

    stanza runner

--- stanza-server/stanza_runner.py
+++ stanza-server/stanza_runner.py
@@ -5,16 +5,29 @@ import sys
 import base64
 import json
 
-if len(sys.argv) != 2:
-  raise Exception("NLP can only take 1 argument")
+def lemmatize(text):
+  nlp = stanza.Pipeline(lang="et", processors="tokenize,pos,lemma", verbose=False)
+  doc = nlp(text)
+  result = []
+  for sentence in doc.sentences:
+    for word in sentence.words:
+      if word.upos != "PUNCT":
+        result.append(word.lemma)
+  return result
 
-text = base64.b64decode(sys.argv[1]).decode("utf-8")
-nlp = stanza.Pipeline(lang="et", processors="tokenize,pos,lemma", verbose=False)
-doc = nlp(text)
-result = []
-for sentence in doc.sentences:
-  for word in sentence.words:
-    if word.upos != "PUNCT":
-      result.append(word.lemma)
+def run_tool(tool, text):
+  if (tool == "lemmatize"):
+    return lemmatize(text)
+  raise Exception(f"Unknown tool {tool}")
 
-print(json.dumps(result))
+def main(args):
+  if len(args) != 2:
+    raise Exception(f"Expected exactly 2 arguments, got: {len(args)}")
+  tool = args[0]
+  text = base64.b64decode(args[1]).decode("utf-8")
+  result = run_tool(tool, text)
+  return json.dumps(result)
+
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  print(main(args))
