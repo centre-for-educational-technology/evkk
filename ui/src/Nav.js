d17a5cc59ae65beaafda0c34c0d3ecf9f994ef40
mikktarvas <mikk.tarvas@gmail.com>
2022-01-28 18:07:49 +0200

    Format

--- ui/src/Nav.js
+++ ui/src/Nav.js
@@ -57,7 +57,7 @@ class Nav extends Component {
               <li className="nav-item">
                 <Link to="/resource" className={'nav-link'}>Ressurss</Link>
               </li>
-			  <li className="nav-item">
+              <li className="nav-item">
                 <a className="nav-link" href="/sonarakendus/paring.html" rel="noopener noreferrer" target="_blank">Päring</a>
               </li>
               {/*
