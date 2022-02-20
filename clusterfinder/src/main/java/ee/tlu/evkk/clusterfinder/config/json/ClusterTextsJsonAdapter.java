ed7d463eb5deeea51f16d5070dd486b349aab3e4
Kristjan Liiva <kristjanliiva@gmail.com>
2022-02-20 14:44:27 +0200

    Clusterfinder improvements
    * Removed unnecessary miscellaneous JSON object parsing for cluster descriptions

--- clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/config/json/ClusterTextsJsonAdapter.java
+++ clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/config/json/ClusterTextsJsonAdapter.java
@@ -18,8 +18,6 @@ public class ClusterTextsJsonAdapter implements JsonDeserializer<Map< String, St
 
   private static final String PUNCTUATION = "punctuation";
 
-  private static final String MISC = "misc";
-
   @Override
   public Map<String, String> deserialize( JsonElement json, Type typeOfT, JsonDeserializationContext context ) throws JsonParseException
   {
@@ -29,7 +27,6 @@ public class ClusterTextsJsonAdapter implements JsonDeserializer<Map< String, St
     deserializeTexts( json, markupsToText, MORFOLOGICAL_ANALYSIS );
     deserializeTexts( json, markupsToText, MORFOSYNTACTIC_ANALYSIS );
     deserializeTexts( json, markupsToText, PUNCTUATION );
-    deserializeTexts( json, markupsToText, MISC );
     return markupsToText;
   }
 
