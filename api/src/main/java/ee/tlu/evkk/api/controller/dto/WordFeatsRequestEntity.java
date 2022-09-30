a9e34794dee7f4cf8924dd6ab74cf9966a0b855e
Harli Kodasma <harlikodasma@gmail.com>
2022-09-30 11:01:30 +0300

    Get rid of Lombok, create manual getters/setters

--- api/src/main/java/ee/tlu/evkk/api/controller/dto/WordFeatsRequestEntity.java
+++ api/src/main/java/ee/tlu/evkk/api/controller/dto/WordFeatsRequestEntity.java
@@ -1,13 +1,23 @@
 package ee.tlu.evkk.api.controller.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class WordFeatsRequestEntity {
 
   private String tekst;
   private String language;
 
+  public String getTekst() {
+    return tekst;
+  }
+
+  public void setTekst(String tekst) {
+    this.tekst = tekst;
+  }
+
+  public String getLanguage() {
+    return language;
+  }
+
+  public void setLanguage(String language) {
+    this.language = language;
+  }
 }
