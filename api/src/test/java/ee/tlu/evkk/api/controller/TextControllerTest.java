fa3821a3296c546b95646fc8984b92c40ade3571
Harli Kodasma <harlikodasma@gmail.com>
2023-01-23 19:39:40 +0200

    Add base for future integration tests, add test for syllabification endpoint

---
+++ api/src/test/java/ee/tlu/evkk/api/controller/TextControllerTest.java
@@ -0,0 +1,35 @@
+package ee.tlu.evkk.api.controller;
+
+import ee.tlu.evkk.api.IntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static ee.tlu.evkk.api.TestUtils.readResourceAsString;
+import static ee.tlu.evkk.api.TestUtils.stringArrayToJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class TextControllerTest extends IntegrationTest {
+
+  @Test
+  @DisplayName("Syllabify text")
+  void syllabifyText() throws Exception {
+    var request = readResourceAsString("SilbidInputRequest.json");
+    var response = readResourceAsString("SilbidInputResponse.json");
+
+    var mockResponse = mockMvc.perform(
+        post("/texts/silbid")
+          .contentType(APPLICATION_JSON)
+          .accept(APPLICATION_JSON)
+          .content(request))
+      .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+    var httpResourceResult = stringArrayToJson(mockResponse);
+    var httpResourceExpected = stringArrayToJson(response);
+
+    assertThat(httpResourceResult)
+      .isEqualTo(httpResourceExpected);
+  }
+}
