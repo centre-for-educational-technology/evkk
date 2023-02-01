455a0d3c26f83dd3827673e737c4a1242d8dc6e0
taavikamarik <taavik@tlu.ee>
2023-02-01 10:57:31 +0200

    Sõnade vahel saab nüüd edasi tagasi liikuda ja paremal kuvab valitud sõna infot.

--- ui/src/elle/tools/wordanalyser/textinput/InputText.js
+++ ui/src/elle/tools/wordanalyser/textinput/InputText.js
@@ -1,31 +1,42 @@
-import {useCallback, useContext, useMemo, useState} from 'react';
+import {useCallback, useContext, useEffect, useMemo, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 import {AnalyseContext} from "../Contexts";
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import {Box} from "@mui/material";
+
+
+function changeNextColor(idNumber, setIdNumber, markedIds, onWordInfo) {
+  if (idNumber < markedIds.length - 1) {
+    onWordInfo(markedIds[idNumber + 1])
+    setIdNumber(idNumber + 1)
+  }
+}
+
+function changePreviousColor(idNumber, setIdNumber, markedIds, onWordInfo) {
+  if (idNumber > 0) {
+    onWordInfo(markedIds[idNumber - 1])
+    setIdNumber(idNumber - 1)
+  }
+}
 
 export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
 
   const analyse = useContext(AnalyseContext)[0];
   const markedIds = [];
   const [idNumber, setIdNumber] = useState(0)
-  let number = 0
 
   const handleWord = useCallback((e) => {
     onWordInfo(e);
     onWordSelect(e);
   }, [onWordInfo, onWordSelect]);
 
-  function changeNextColor() {
-    if (number < markedIds.length) {
-      document.getElementById(markedIds[number]).classList.replace("blue", "marked")
-
-
-      document.getElementById(markedIds[number + 1]).classList.replace("marked", "blue")
-      number++
+  useEffect(() => {
+    setIdNumber(0)
+    if (markedIds.length > 0) {
+      onWordInfo(markedIds[0])
     }
-
-  }
+  }, [onMarkWords])
 
   const updatedText = useMemo(() => {
     let analysedWords = analyse.wordsOrig;
@@ -39,6 +50,7 @@ export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
         for (const element of onMarkWords) {
           if (ids[i] === element) {
             isMarked = true;
+            markedIds.push(ids[i])
           }
         }
         if (index > 0) {
@@ -59,17 +71,14 @@ export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
             }
           }
           content.push(sequence);
-
           if (isMarked) {
-            if (markedIds.length === 0) {
+            if (ids[i] === markedIds[idNumber]) {
               content.push(<span id={ids[i]}
                                  className="word blue"
                                  key={uuidv4()}
                                  onClick={(e) => {
                                    handleWord(e.target.id)
                                  }}>{analysedWords[i]}</span>);
-              markedIds.push(ids[i])
-
             } else {
               content.push(<span id={ids[i]}
                                  className="word marked"
@@ -77,9 +86,7 @@ export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
                                  onClick={(e) => {
                                    handleWord(e.target.id)
                                  }}>{analysedWords[i]}</span>);
-              markedIds.push(ids[i])
             }
-
           } else {
             content.push(<span id={ids[i]}
                                className="word"
@@ -89,14 +96,14 @@ export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
           text = text.substring(index + analysedWords[i].length, text.length);
         } else {
           if (isMarked) {
-            if (markedIds.length === 0) {
+            if (ids[i] === markedIds[idNumber]) {
               content.push(<span id={ids[i]}
                                  className="word blue"
                                  key={uuidv4()}
                                  onClick={(e) => {
                                    handleWord(e.target.id)
                                  }}>{analysedWords[i]}</span>);
-              markedIds.push(ids[i])
+
 
             } else {
               content.push(<span id={ids[i]}
@@ -105,10 +112,7 @@ export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
                                  onClick={(e) => {
                                    handleWord(e.target.id)
                                  }}>{analysedWords[i]}</span>);
-              markedIds.push(ids[i])
             }
-
-
           } else {
             content.push(<span id={ids[i]}
                                className="word"
@@ -121,15 +125,18 @@ export const InputText = ({onMarkWords, onWordSelect, onWordInfo}) => {
     }
     content.push(text);
     return <>{content}</>;
-  }, [analyse, onMarkWords, handleWord, markedIds]);
-
-  console.log(markedIds)
+  }, [analyse, onMarkWords, handleWord, markedIds, idNumber]);
 
   return (
     <div className="textInputDiv">
       {updatedText}
-      <div style={{position: "absolute", left: "45%", top: "34%"}}><KeyboardArrowLeftIcon
-        cursor={"pointer"}/><KeyboardArrowRightIcon cursor={"pointer"} onClick={changeNextColor}/></div>
+      <div style={{position: "absolute", left: "45%", top: "34%", display: "flex"}}>
+        {idNumber > 0 ? <KeyboardArrowLeftIcon cursor={"pointer"}
+                                               onClick={() => changePreviousColor(idNumber, setIdNumber, markedIds, onWordInfo)}/> :
+          <Box width={"24px"} height={"24px"}></Box>}
+        {idNumber < markedIds.length - 1 ? <KeyboardArrowRightIcon cursor={"pointer"}
+                                                                   onClick={() => changeNextColor(idNumber, setIdNumber, markedIds, onWordInfo)}/> : null}
+      </div>
     </div>
   )
 }
