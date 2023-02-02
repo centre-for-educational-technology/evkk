05463d9b384c5d0f91b347bd4969341bc521297e
Jaagup Kippar <jaagup@tlu.ee>
2023-02-02 22:13:00 +0200

    Tekstide üleslaadimine (#141)
    * Initial functionality of adding texts
    
    * backend validation
    
    * meta_trigger
    
    ---------
    
    Co-authored-by: Harli Kodasma <harlikodasma@gmail.com>

---
+++ api/src/main/java/ee/tlu/evkk/api/controller/dto/AddingRequestEntity.java
@@ -0,0 +1,170 @@
+package ee.tlu.evkk.api.controller.dto;
+import javax.validation.constraints.NotBlank;
+
+public class AddingRequestEntity {
+
+  @NotBlank
+  private String pealkiri;
+  private String kirjeldus;
+  @NotBlank
+  private String sisu;
+  private String liik;
+  private String oppematerjal;
+  private String akadOppematerjal;
+  private String mitteakadAlamliik;
+  private String akadAlamliik;
+  private String tekstiAutor;
+  private String autoriVanus;
+  private String autoriSugu;
+  private String autoriOppeaste;
+  private String autoriHaridus;
+  private String autoriEriala;
+  private String autoriEmakeel;
+  private String autoriMuudKeeled;
+  private String autoriElukohariik;
+  private String nousOlek;
+
+  public String getPealkiri() {
+    return pealkiri;
+  }
+
+  public void setPealkiri(String pealkiri) {
+    this.pealkiri = pealkiri;
+  }
+
+  public String getKirjeldus() {
+    return kirjeldus;
+  }
+
+  public void setKirjeldus(String kirjeldus) {
+    this.kirjeldus = kirjeldus;
+  }
+
+  public String getSisu() {
+    return sisu;
+  }
+
+  public void setSisu(String sisu) {
+    this.sisu = sisu;
+  }
+
+  public String getLiik() {
+    return liik;
+  }
+
+  public void setLiik(String liik) {
+    this.liik = liik;
+  }
+
+  public String getOppematerjal() {
+    return oppematerjal;
+  }
+
+  public void setOppematerjal(String oppematerjal) {
+    this.oppematerjal = oppematerjal;
+  }
+
+  public String getAkadOppematerjal() {
+    return akadOppematerjal;
+  }
+
+  public void setAkadOppematerjal(String akadOppematerjal) {
+    this.akadOppematerjal = akadOppematerjal;
+  }
+
+  public String getMitteakadAlamliik() {
+    return mitteakadAlamliik;
+  }
+
+  public void setMitteakadAlamliik(String mitteakadAlamliik) {
+    this.mitteakadAlamliik = mitteakadAlamliik;
+  }
+
+  public String getAkadAlamliik() {
+    return akadAlamliik;
+  }
+
+  public void setAkadAlamliik(String akadAlamliik) {
+    this.akadAlamliik = akadAlamliik;
+  }
+
+  public String getTekstiAutor() {
+    return tekstiAutor;
+  }
+
+  public void setTekstiAutor(String tekstiAutor) {
+    this.tekstiAutor = tekstiAutor;
+  }
+
+  public String getAutoriVanus() {
+    return autoriVanus;
+  }
+
+  public void setAutoriVanus(String autoriVanus) {
+    this.autoriVanus = autoriVanus;
+  }
+
+  public String getAutoriSugu() {
+    return autoriSugu;
+  }
+
+  public void setAutoriSugu(String autoriSugu) {
+    this.autoriSugu = autoriSugu;
+  }
+
+  public String getAutoriOppeaste() {
+    return autoriOppeaste;
+  }
+
+  public void setAutoriOppeaste(String autoriOppeaste) {
+    this.autoriOppeaste = autoriOppeaste;
+  }
+
+  public String getAutoriHaridus() {
+    return autoriHaridus;
+  }
+
+  public void setAutoriHaridus(String autoriHaridus) {
+    this.autoriHaridus = autoriHaridus;
+  }
+
+  public String getAutoriEriala() {
+    return autoriEriala;
+  }
+
+  public void setAutoriEriala(String autoriEriala) {
+    this.autoriEriala = autoriEriala;
+  }
+
+  public String getAutoriEmakeel() {
+    return autoriEmakeel;
+  }
+
+  public void setAutoriEmakeel(String autoriEmakeel) {
+    this.autoriEmakeel = autoriEmakeel;
+  }
+
+  public String getAutoriMuudKeeled() {
+    return autoriMuudKeeled;
+  }
+
+  public void setAutoriMuudKeeled(String autoriMuudKeeled) {
+    this.autoriMuudKeeled = autoriMuudKeeled;
+  }
+
+  public String getAutoriElukohariik() {
+    return autoriElukohariik;
+  }
+
+  public void setAutoriElukohariik(String autoriElukohariik) {
+    this.autoriElukohariik = autoriElukohariik;
+  }
+
+  public String getNousOlek() {
+    return nousOlek;
+  }
+
+  public void setNousOlek(String nousOlek) {
+    this.nousOlek = nousOlek;
+  }
+}
