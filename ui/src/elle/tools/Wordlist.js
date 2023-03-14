49fcc4207d2566744f0b20aaaa83a4fbf0a89f7c
Harli Kodasma <harlikodasma@gmail.com>
2023-03-14 18:45:24 +0200

    106 | Redirect away from tool when no texts are selected

--- ui/src/elle/tools/Wordlist.js
+++ ui/src/elle/tools/Wordlist.js
@@ -1,4 +1,16 @@
+import {queryStore} from "../store/QueryStore";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 export default function Wordlist() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!queryStore.getState()) {
+      navigate(-1);
+    }
+  }, [navigate]);
 
   return (
     <></>
