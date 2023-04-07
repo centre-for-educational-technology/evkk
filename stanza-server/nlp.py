b92f1f3c0354e6d31c081f975e1ee1c260a80ebc
Jaagup Kippar <jaagup@tlu.ee>
2023-04-07 20:59:01 +0300

    Lemmade tuvastamine stanza conllu väljundis - kõik protsessorid sisse

--- stanza-server/nlp.py
+++ stanza-server/nlp.py
@@ -1,9 +1,11 @@
 import stanza
 
+nlp_all = stanza.Pipeline(lang='et')
 nlp_tpl = stanza.Pipeline(lang='et', processors='tokenize,pos,lemma')
 nlp_tp = stanza.Pipeline(lang='et', processors='tokenize,pos')
 nlp_t = stanza.Pipeline(lang='et', processors='tokenize')
 
+nlp_ru_all = stanza.Pipeline(lang='ru')
 nlp_ru_tpl = stanza.Pipeline(lang='ru', processors='tokenize,pos,lemma')
 nlp_ru_tp = stanza.Pipeline(lang='ru', processors='tokenize,pos')
 nlp_ru_t = stanza.Pipeline(lang='ru', processors='tokenize')
