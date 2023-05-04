a1e1e7ac3b313b839a5911cce949732103f55a23
Harli Kodasma <70939399+harlikodasma@users.noreply.github.com>
2023-05-04 21:52:18 +0300

    185 | Fix text processor logic (#176)

--- core/src/main/java/ee/tlu/evkk/core/service/TextProcessorService.java
+++ core/src/main/java/ee/tlu/evkk/core/service/TextProcessorService.java
@@ -15,6 +15,7 @@ import org.springframework.util.MultiValueMap;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -42,7 +43,7 @@ public class TextProcessorService {
     this.textProcessorExecutor = textProcessorExecutor;
   }
 
-  public Json processText(Type type, UUID textId) {
+  public void processText(Type type, UUID textId) {
     Optional<Text> text = textDao.findById(textId);
     if (text.isEmpty()) throw new RuntimeException("Text not found: " + textId); //TODO: use business exception
 
@@ -51,32 +52,43 @@ public class TextProcessorService {
 
     Optional<TextProcessorResult> found = textProcessorResultDao.findResultForTypeAndVersion(hash, type.toString(), version);
     if (found.isPresent()) {
-      return found.get().getResult();
+      return;
     }
 
     Json result;
+    String finalText = text.get().getContent()
+      .replace("\\n", " ")
+      .replace("\\t", " ");
+
     try {
-      result = jsonFactory.createFromObject(textProcessorExecutor.execute(type, buildProcessorContext(textId), text.get().getContent()));
+      result = jsonFactory.createFromObject(textProcessorExecutor.execute(type, buildProcessorContext(textId), finalText));
     } catch (Exception ex) {
+      textProcessorResultDao.upsert(buildTextProcessorResult(hash, buildEmptyJson(), type.toString(), version));
       throw new RuntimeException("Unable to process textId " + textId + " using " + type + " processor", ex);
     }
+    textProcessorResultDao.upsert(buildTextProcessorResult(hash, result, type.toString(), version));
+  }
 
+  private Json buildEmptyJson() {
+    return jsonFactory.createFromObject(Map.of("contents", ""));
+  }
+
+  private TextProcessorResult buildTextProcessorResult(String hash, Json result, String type, Long version) {
     TextProcessorResult textProcessorResult = new TextProcessorResult();
     textProcessorResult.setTextHash(hash);
     textProcessorResult.setResult(result);
-    textProcessorResult.setType(type.toString());
+    textProcessorResult.setType(type);
     textProcessorResult.setVersion(version);
-
-    textProcessorResultDao.upsert(textProcessorResult);
-
-    return result;
+    return textProcessorResult;
   }
 
   private Context buildProcessorContext(UUID textId) {
     MultiValueMap<String, String> textProperties = textPropertyService.getTextProperties(textId);
     Context context = Context.newInstance();
-    if (textProperties.containsKey("failinimi")) context = context.withOriginalFileName(textProperties.getFirst("failinimi"));
-    if (textProperties.containsKey("tekstikeel")) context = context.withLanguageCode(textProperties.getFirst("tekstikeel"));
+    if (textProperties.containsKey("failinimi"))
+      context = context.withOriginalFileName(textProperties.getFirst("failinimi"));
+    if (textProperties.containsKey("tekstikeel"))
+      context = context.withLanguageCode(textProperties.getFirst("tekstikeel"));
     context = context.withFallbackFileName(textId + ".txt");
     return context;
   }
