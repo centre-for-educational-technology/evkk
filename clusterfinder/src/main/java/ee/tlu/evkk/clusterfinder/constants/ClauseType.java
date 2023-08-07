61febed83f62605002d9676d99522cd98ba471fd
taavi <taavik@tlu.ee>
2023-08-07 10:52:42 +0300

    Dropdown järjekorra muutmine. Dropdown tekstide muutmine.

--- clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/constants/ClauseType.java
+++ clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/constants/ClauseType.java
@@ -1,44 +1,38 @@
 package ee.tlu.evkk.clusterfinder.constants;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public enum ClauseType
-{
-  ALL( "ALL", "clause.type.all" ),
-  PREDICATE( "F", "clause.type.predicate" ),
-  BASIS( "@SUBJ", "clause.type.basis" ),
-  OBJECTIVE( "@OBJ", "clause.type.objective" ),
-  PEDICATE( "@PRD", "clause.type.pedicate" ),
-  ADVERBIAL( "@ADVL", "clause.type.adverbial" ),
-  ATTRIBUTE( "AT", "clause.type.attribute" ),
-  QUANTIFIER_MODIFIER( "QM", "clause.type.quantifier.modifier"),
-  ADPOSITION_APPURTENANT( "AP", "clause.type.adposition.appurtenant" ),
-  CONJUNCTIVE_WORD( "@J", "clause.type.conjunctive.word" ),
-  EXCLAMATION( "@B", "clause.type.exclamation" );
+@Getter
+public enum ClauseType {
+    ALL("ALL", "clause.type.all"),
+    PREDICATE("F", "clause.type.predicate"),
+    BASIS("@SUBJ", "clause.type.basis"),
+    OBJECTIVE("@OBJ", "clause.type.objective"),
+    PEDICATE("@PRD", "clause.type.pedicate"),
+    ATTRIBUTE("AT", "clause.type.attribute"),
+    ADVERBIAL("@ADVL", "clause.type.adverbial"),
+    CONJUNCTIVE_WORD("@J", "clause.type.conjunctive.word"),
+    EXCLAMATION("@B", "clause.type.exclamation"),
+    QUANTIFIER_MODIFIER("QM", "clause.type.quantifier.modifier"),
+    ADPOSITION_APPURTENANT("AP", "clause.type.adposition.appurtenant");
 
-  private final String value;
 
-  private final String labelKey;
+    private static final Map<String, ClauseType> VALUE_TO_CLAUSE_MAP = Arrays.stream(ClauseType.values())
+            .collect(Collectors.toMap(ClauseType::getValue, c -> c));
+    private final String value;
+    private final String labelKey;
 
-  private static final Map< String, ClauseType > VALUE_TO_CLAUSE_MAP = Arrays.stream(ClauseType.values())
-    .collect(Collectors.toMap(ClauseType::getValue, c -> c ));
+    ClauseType(String value, String labelKey) {
+        this.value = value;
+        this.labelKey = labelKey;
+    }
 
-  ClauseType(String value, String labelKey) {
-    this.value = value;
-    this.labelKey = labelKey;
-  }
+    public static ClauseType getByValue(String value) {
+        return VALUE_TO_CLAUSE_MAP.getOrDefault(value, ALL);
+    }
 
-  public String getValue() {
-    return value;
-  }
-
-  public String getLabelKey() {
-    return labelKey;
-  }
-
-  public static ClauseType getByValue( String value ) {
-    return VALUE_TO_CLAUSE_MAP.getOrDefault( value, ALL );
-  }
 }
--- clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/template/ClusterFinderTemplate.java
+++ clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/template/ClusterFinderTemplate.java
@@ -1,63 +1,60 @@
 package ee.tlu.evkk.clusterfinder.template;
 
 import ee.tlu.evkk.clusterfinder.client.EvkkClient;
-import ee.tlu.evkk.clusterfinder.constants.*;
+import ee.tlu.evkk.clusterfinder.constants.AjaxConsts;
+import ee.tlu.evkk.clusterfinder.constants.ClauseType;
+import ee.tlu.evkk.clusterfinder.constants.TemplateConstants;
+import ee.tlu.evkk.clusterfinder.constants.WordType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
+
 
 @Controller
 public class ClusterFinderTemplate {
 
-  private static final Map< String, String > AJAX_URLS = Map.ofEntries(
-    Map.entry( "clusterText", AjaxConsts.CLUSTER_TEXT),
-    Map.entry( "clusterDownload", AjaxConsts.CLUSTER_DOWNLOAD ),
-    Map.entry( "uploadFile", AjaxConsts.FILE_UPLOAD )
-  );
+    private static final Map<String, String> AJAX_URLS = Map.ofEntries(
+            Map.entry("clusterText", AjaxConsts.CLUSTER_TEXT),
+            Map.entry("clusterDownload", AjaxConsts.CLUSTER_DOWNLOAD),
+            Map.entry("uploadFile", AjaxConsts.FILE_UPLOAD)
+    );
 
-  private final EvkkClient evkkClient;
+    private final EvkkClient evkkClient;
 
-  public ClusterFinderTemplate(EvkkClient evkkClient) {
-    this.evkkClient = evkkClient;
-  }
+    public ClusterFinderTemplate(EvkkClient evkkClient) {
+        this.evkkClient = evkkClient;
+    }
 
-  @GetMapping( "/" )
-  public String render( Model model,
-                        @RequestParam( value = "evkkSessionToken", required = false ) String sessionToken,
-                        @RequestParam( value = "userFileId", required = false ) String userFileId )
-  {
-    model.addAttribute( "userFiles", sessionToken != null ? evkkClient.listFiles( sessionToken ) : List.of() );
-    model.addAttribute( "selectedFile", userFileId );
-    model.addAttribute( "formId", UUID.randomUUID().toString() );
-    return TemplateConstants.CLUSTER_SEARCH;
-  }
+    @GetMapping("/")
+    public String render(Model model,
+                         @RequestParam(value = "evkkSessionToken", required = false) String sessionToken,
+                         @RequestParam(value = "userFileId", required = false) String userFileId) {
+        model.addAttribute("userFiles", sessionToken != null ? evkkClient.listFiles(sessionToken) : List.of());
+        model.addAttribute("selectedFile", userFileId);
+        model.addAttribute("formId", UUID.randomUUID().toString());
+        return TemplateConstants.CLUSTER_SEARCH;
+    }
 
-  @ModelAttribute("ajaxUrls")
-  public Map< String, String > getAjaxUrls()
-  {
-    return AJAX_URLS;
-  }
+    @ModelAttribute("ajaxUrls")
+    public Map<String, String> getAjaxUrls() {
+        return AJAX_URLS;
+    }
 
-  @ModelAttribute("wordTypes")
-  public Map< String, String > getWordTypes()
-  {
-    return Arrays.stream(WordType.values())
-                 .collect(Collectors.toMap(WordType::getValue, WordType::getLabelKey));
-  }
+    @ModelAttribute("wordTypes")
+    public Map<String, String> getWordTypes() {
+        return Arrays.stream(WordType.values())
+                .collect(Collectors.toMap(WordType::getValue, WordType::getLabelKey, (existing, replacement) -> existing, LinkedHashMap::new));
+    }
 
-  @ModelAttribute("clauseTypes")
-  public Map< String, String > getClauseTypes()
-  {
-    return Arrays.stream(ClauseType.values())
-                 .collect(Collectors.toMap(ClauseType::getValue, ClauseType::getLabelKey));
-  }
+    @ModelAttribute("clauseTypes")
+    public Map<String, String> getClauseTypes() {
+        return Arrays.stream(ClauseType.values())
+                .collect(Collectors.toMap(ClauseType::getValue, ClauseType::getLabelKey, (existing, replacement) -> existing, LinkedHashMap::new));
+    }
 
 }
