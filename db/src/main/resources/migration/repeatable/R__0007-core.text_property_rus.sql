f370171695a655fb3a53119bab99d1af4bc114aa
Jaagup Kippar <jaagup@tlu.ee>
2023-09-25 00:21:41 +0300

    Parandused (#200)

---
+++ db/src/main/resources/migration/repeatable/R__0007-core.text_property_rus.sql
@@ -0,0 +1,26 @@
+UPDATE core.text_property
+SET property_value='cFOoRQekA'
+WHERE property_name = 'korpus'
+  AND text_id = '3c7354e1-1d1a-4224-8879-2d13933a54cc';
+UPDATE core.text_property
+SET property_value='cFOoRQekA'
+WHERE property_name = 'korpus'
+  AND text_id = '5adb7ef2-b5d8-4d82-9f3b-ed0bbab18332';
+UPDATE core.text_property
+SET property_value='cFOoRQekA'
+WHERE property_name = 'korpus'
+  AND text_id = '73e489dc-18b2-4a94-8116-845f7e6dbe13';
+
+
+UPDATE core.text_property
+SET property_value='k2eesti_loovkirjutis'
+WHERE property_name = 'tekstityyp'
+  AND text_id = '3c7354e1-1d1a-4224-8879-2d13933a54cc';
+UPDATE core.text_property
+SET property_value='k2eesti_loovkirjutis'
+WHERE property_name = 'tekstityyp'
+  AND text_id = '5adb7ef2-b5d8-4d82-9f3b-ed0bbab18332';
+UPDATE core.text_property
+SET property_value='k2eesti_loovkirjutis'
+WHERE property_name = 'tekstityyp'
+  AND text_id = '73e489dc-18b2-4a94-8116-845f7e6dbe13';
