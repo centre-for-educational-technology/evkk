75248561869138536bf968ebfbeabc309888fb43
taavikamarik <taavik@tlu.ee>
2023-09-25 15:50:54 +0300

    Koodi optimeerimine

--- ui/src/elle/components/InfoElement.js
+++ ui/src/elle/components/InfoElement.js
@@ -5,113 +5,103 @@ import word_analyzer_video from '../resources/videos/word_analyzer_full.webm'
 import {useTranslation} from "react-i18next";
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import InfoElementTabCard from "./InfoElementTabCard";
+import {ElleOuterDivStyle} from "../utils/constants";
 
 const InfoElement = () => {
 
   const {t} = useTranslation();
-  const [visibleButton, setVisibleButton] = useState("tekstihindaja-btn")
-  const [visibleBox, setVisibleBox] = useState("tekstihindaja-btn-box")
-  const [summaryKey, setSummaryKey] = useState(1)
+  const [openTab, setOpentab] = useState("corrector-tab-card");
 
   const descriptionText = {
-    1: <p><b>Tekstihindajaga</b> {t('homepage_box_corrector_content')}</p>,
-    2: <p><b>Tekstipäringu</b> {t('homepage_box_query_content')}</p>,
-    3: <p><b>Sõnaloendi</b> {t('homepage_box_wordlist_content')}</p>,
-    4: <p><b>Sõna kontekstis</b> {t('homepage_box_concordances_content')}</p>,
-    5: <p><b>Naabersõnade</b> {t('homepage_box_neighbouring_words_content')}</p>,
-    6: <p><b>Mustrileidjaga</b> {t('homepage_box_clusters_content')}</p>,
-    7: <p><b>Sõnaanalüsaatoriga</b> {t('homepage_box_word_analysis_content')}</p>
-  }
-  const toggleBtnClass = async (e, key) => {
-    const activeBtn = await document.getElementById(visibleButton);
-    const activeBox = await document.getElementById(visibleBox);
-    const id = e.target.id;
-    const hoveredBtn = await document.getElementById(id);
-    const hoveredInfoBox = await document.getElementById(id + "-box");
-
-    if (hoveredBtn !== null) {
-      if (hoveredBtn.classList.contains('btn-visible')) {
-
-      } else if (hoveredBtn.classList.contains('btn-invisible')) {
-        setSummaryKey(key);
-        activeBtn.classList.remove('btn-visible');
-        activeBox.classList.remove('info-box-slide-visible')
-        activeBtn.classList.add('btn-invisible');
-        activeBox.classList.add('info-box-slide-invisible')
-        setVisibleButton(hoveredBtn.id);
-        setVisibleBox(hoveredBtn.id + '-box')
-        hoveredBtn.classList.remove('btn-invisible');
-        hoveredInfoBox.classList.remove('info-box-slide-invisible')
-        hoveredBtn.classList.add('btn-visible');
-        hoveredInfoBox.classList.add('info-box-slide-visible')
-      }
-    }
-  }
+    1: <p><b>{t('homepage_box_corrector_title')}</b> {t('homepage_box_corrector_content')}</p>,
+    2: <p><b>{t('homepage_box_query_title')}</b> {t('homepage_box_query_content')}</p>,
+    3: <p><b>{t('homepage_box_wordlist_title')}</b> {t('homepage_box_wordlist_content')}</p>,
+    4: <p><b>{t('homepage_box_concordances_title')}</b> {t('homepage_box_concordances_content')}</p>,
+    5: <p><b>{t('homepage_box_neighbouring_words_title')}</b> {t('homepage_box_neighbouring_words_content')}</p>,
+    6: <p><b>{t('homepage_box_clusters_title')}</b> {t('homepage_box_clusters_content')}</p>,
+    7: <p><b>{t('homepage_box_word_analysis_title')}</b> {t('homepage_box_word_analysis_content')}</p>
+  };
 
   return (
-    <Box id="elle-tools" className="info-element-container">
+    <Box id="elle-tools" sx={ElleOuterDivStyle} className="info-element-container">
       <Box className="info-element-inner">
         <Box className="info-tool-title">
-          <h2>ELLE tööriistad</h2>
+          <h2>{t('tools_title')}</h2>
         </Box>
         <Box className="elle-tools">
-          <InfoElementTabCard toolID={"tekstihindaja-btn"} toolTitile={"Tekstihindaja"}
-                              toolInnerId={"tekstihindaja-btn-box"} toolKey={1}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
-          <InfoElementTabCard toolID={"tekstiparing-btn"} toolTitile={"Tekstipäring"}
-                              toolInnerId={"tekstiparing-btn-box"} toolKey={2}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
-          <InfoElementTabCard toolID={"sonaloend-btn"} toolTitile={"Sõnaloend"} toolInnerId={"sonaloend-btn-box"}
-                              toolKey={3}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
-          <InfoElementTabCard toolID={"word-context-btn"} toolTitile={"Sõna kontekstis"}
-                              toolInnerId={"word-context-btn-box"} toolKey={4}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
-          <InfoElementTabCard toolID={"neighbour-word-btn"} toolTitile={"Naabersõnad"}
-                              toolInnerId={"neighbour-word-btn-box"} toolKey={5}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
-          <InfoElementTabCard toolID={"cluster-finder-btn"} toolTitile={"Mustrileidja"}
-                              toolInnerId={"cluster-finder-btn-box"} toolKey={6}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
-          <InfoElementTabCard toolID={"word-analyser-btn"} toolTitile={"Sõnaanalüsaator"}
-                              toolInnerId={"word-analyser-btn-box"} toolKey={7}
-                              description={descriptionText[summaryKey]} toggleBtnClass={toggleBtnClass()}/>
+          <InfoElementTabCard
+            toolID={"corrector-tab-card"}
+            toolTitile={t('common_corrector')}
+            tabOpen={openTab}
+            toolInnerId={"corrector-info-popup"}
+            description={descriptionText[1]}
+            setOpenTab={setOpentab}
+          />
+          <InfoElementTabCard
+            toolID={"tekstiparing-btn"}
+            toolTitile={"Tekstipäring"}
+            tabOpen={openTab}
+            toolInnerId={"tekstiparing-btn-box"}
+            description={descriptionText[2]}
+            setOpenTab={setOpentab}
+          />
+          <InfoElementTabCard
+            toolID={"wordlist-tab-card"}
+            toolTitile={t('common_wordlist')}
+            toolInnerId={"wordlist-info-popup"}
+            tabOpen={openTab}
+            description={descriptionText[3]}
+            setOpenTab={setOpentab}
+          />
+          <InfoElementTabCard
+            toolID={"word-context-tab-card"}
+            toolTitile={t('common_word_in_context')}
+            tabOpen={openTab}
+            toolInnerId={"word-context-info-popup"}
+            description={descriptionText[4]}
+            setOpenTab={setOpentab}
+          />
+          <InfoElementTabCard
+            toolID={"neighbour-word-tab-card"}
+            toolTitile={t('common_neighbouring_words')}
+            tabOpen={openTab}
+            toolInnerId={"neighbour-word-info-popup"}
+            description={descriptionText[5]}
+            setOpenTab={setOpentab}
+          />
+          <InfoElementTabCard
+            toolID={"cluster-finder-tab-card"}
+            toolTitile={t('common_clusters')}
+            tabOpen={openTab}
+            toolInnerId={"cluster-finder-info-popup"}
+            description={descriptionText[6]}
+            setOpenTab={setOpentab}
+          />
+          <InfoElementTabCard
+            toolID={"word-analyser-tab-card"}
+            toolTitile={"Sõnaanalüsaator"}
+            tabOpen={openTab}
+            toolInnerId={"word-analyser-info-popup"}
+            description={descriptionText[7]}
+            setOpenTab={setOpentab}
+          />
         </Box>
-        {/*<Box className="tool-buttons">
-          <Box className="tool-button" bgcolor={"#ff758f80"} width={"13%"}>
-            <p>Tekstihindaja</p>
-          </Box>
-          <Box className="tool-button" bgcolor={"#fb610780"}>
-            <p>Tekstipäring</p>
-          </Box>
-          <Box className="tool-button" bgcolor={"#ffff3f80"}>
-            <p>Sõnaloend</p>
-          </Box>
-          <Box className="tool-button" bgcolor={"#80b91880"}>
-            <p>Sõna kontekstis</p>
-          </Box>
-          <Box className="tool-button" bgcolor={"#00b4d880"}>
-            <p>Naabersõnad</p>
-          </Box>
-          <Box className="tool-button" bgcolor={"#deaaff80"}>
-            <p>Mustrileidja</p>
-          </Box>
-          <Box className="tool-button" bgcolor={"#343a4080"}>
-            <p>Sõnaanalüsaator</p>
-          </Box>
-        </Box>*/}
         <Box className="info-video">
-          <video className={"info-video-styling"} controls src={word_analyzer_video} autoPlay width={"100%"}
-                 preload></video>
-          <Box className="video-overlay"><PlayCircleIcon className="play-button-info"/>
-            <Box fontSize={"x-large"} position={"relative"} top={"20%"} fontWeight={"bold"}
-                 color={"rgb(204, 168, 253)"}><p>VAATA TUTVUSTAVAT
-              VIDEOT</p>
-            </Box></Box>
+          <video
+            className={"info-video-styling w-100"}
+            controls
+            src={word_analyzer_video}
+            autoPlay
+            preload
+          />
+          <Box className="video-overlay">
+            <PlayCircleIcon className="play-button-info"/>
+            <Box className="position-relative font-weight-bold elle-medium-text tools-video-text">
+              <p className="text-uppercase">{t('tools_video_text')}</p>
+            </Box>
+          </Box>
         </Box>
-
       </Box>
-
     </Box>
   );
 };
--- ui/src/elle/components/InfoElementTabCard.js
+++ ui/src/elle/components/InfoElementTabCard.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {Box, Button} from "@mui/material";
 import './styles/InfoElementTabCard.css'
-
+import {ButtonStyleSmall} from "../utils/constants";
+import {useTranslation} from "react-i18next";
 
 const InfoElementTabCard = (props) => {
 
+  const {t} = useTranslation()
+
   return (
-    <Box id={props.toolID} onMouseEnter={(e) => props.toggleBtnClass(e, props.toolKey)}
-         className="btn-invisible">
+    <Box id={props.toolID} onMouseEnter={(e) => props.setOpenTab(props.toolID)}
+         className={props.tabOpen === props.toolID ? "btn-visible" : "btn-invisible"}>
       <p className="tool-button-text">{props.toolTitile}</p>
-      <Box id={props.toolInnerId} className="info-box-slide-invisible" style={{top: "5%"}}>
+      <Box id={props.toolInnerId}
+           className={props.tabOpen === props.toolID ? "info-box-slide-visible" : "info-box-slide-invisible"}>
         <Box className="btn-box-inner">
           <Box className="btn-box-inner-inner">
             {props.description}
-            <Button variant={"contained"} sx={{bgcolor: "#9C27B0", fontWeight: "bold", borderRadius: "10px"}}
-                    size={"small"}>Uuri lähemalt</Button>
+            <Button variant={"contained"} sx={ButtonStyleSmall} size={"small"}>
+              {t('find_out_more')}
+            </Button>
           </Box>
         </Box>
       </Box>
