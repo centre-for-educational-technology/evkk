c7d47881b8045b2613ba273a86886db73885de92
Taavi Kamarik <90038572+TaaviKamarik@users.noreply.github.com>
2023-09-25 10:56:17 +0300

    ELLE-171 | clusterfinder menu changes (#197)
    * Dropdown järjekorra muutmine. Dropdown tekstide muutmine.
    
    * Muutsin sulud kandfilisteks
    
    * Koodi optimeerimine.
    
    * Koodi optimeerimine.
    
    * Koodi optimeerimine.

--- clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/constants/WordType.java
+++ clusterfinder/src/main/java/ee/tlu/evkk/clusterfinder/constants/WordType.java
@@ -1,44 +1,37 @@
 package ee.tlu.evkk.clusterfinder.constants;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public enum WordType
-{
-  ALL( "ALL", "word.type.all" ),
-  VERB( "V", "word.type.verb" ),
-  SUBJECT( "S", "word.type.subject" ),
-  ADJECTIVE( "A", "word.type.adjective" ),
-  PRONOUN( "P", "word.type.pronoun" ),
-  NUMERAL( "N", "word.type.numeral" ),
-  ADVERB( "D", "word.type.adverb" ),
-  CONJUNCTION( "J", "word.type.conjunction" ),
-  ADPOSITION( "K", "word.type.adposition" ),
-  ABBREVIATION( "Y", "word.type.abbreviation" ),
-  PUNCTUATION( "Z", "word.type.punctuation" );
+@Getter
+public enum WordType {
+    ALL("ALL", "word.type.all"),
+    VERB("V", "word.type.verb"),
+    SUBJECT("S", "word.type.subject"),
+    ADJECTIVE("A", "word.type.adjective"),
+    PRONOUN("P", "word.type.pronoun"),
+    NUMERAL("N", "word.type.numeral"),
+    ADVERB("D", "word.type.adverb"),
+    ADPOSITION("K", "word.type.adposition"),
+    CONJUNCTION("J", "word.type.conjunction"),
+    ABBREVIATION("Y", "word.type.abbreviation"),
+    PUNCTUATION("Z", "word.type.punctuation");
 
-  private final String labelKey;
+    private static final Map<String, WordType> VALUE_TO_WORD_MAP = Arrays.stream(WordType.values())
+            .collect(Collectors.toMap(WordType::getValue, w -> w));
+    private final String labelKey;
+    private final String value;
 
-  private final String value;
+    WordType(String value, String labelKey) {
+        this.value = value;
+        this.labelKey = labelKey;
+    }
 
-  private static final Map< String, WordType > VALUE_TO_WORD_MAP = Arrays.stream( WordType.values() )
-    .collect( Collectors.toMap( WordType::getValue, w -> w ) );
+    public static WordType getByValue(String value) {
+        return VALUE_TO_WORD_MAP.getOrDefault(value, ALL);
+    }
 
-  WordType( String value, String labelKey ) {
-    this.value = value;
-    this.labelKey = labelKey;
-  }
-
-  public String getLabelKey() {
-    return labelKey;
-  }
-
-  public String getValue() {
-    return value;
-  }
-
-  public static WordType getByValue( String value ) {
-    return VALUE_TO_WORD_MAP.getOrDefault( value, ALL );
-  }
 }
