96525a64f5982b7c82ef0178673b20f41bf7c82e
taavikamarik <taavik@tlu.ee>
2023-10-11 15:22:25 +0300

    Kõik requestid tehtud peale piltide suuruste.

--- ui/src/elle/components/IntegrationFrame.js
+++ ui/src/elle/components/IntegrationFrame.js
@@ -5,18 +5,18 @@ import {Box} from "@mui/material";
 
 const IntegrationFrame = ({integrationName}) => {
 
-    const path = useSelector(state => selectIntegrationPath(integrationName)(state), []);
-    const [height, setHeight] = useState("");
+  const path = useSelector(state => selectIntegrationPath(integrationName)(state), []);
+  const [height, setHeight] = useState("");
 
-    window.addEventListener('message', function (event) {
-        setHeight((event.data + 100) + "px");
-    });
+  window.addEventListener('message', function (event) {
+    setHeight((event.data + 100) + "px");
+  });
 
-    return (
-        <Box className="embed-responsive embed-responsive-21by9 overflow-hidden" height={height}>
-            <iframe src={path} title={integrationName}/>
-        </Box>
-    );
+  return (
+    <Box className="embed-responsive embed-responsive-21by9 overflow-hidden" height={height} width={"100%"}>
+      <iframe src={path} title={integrationName}/>
+    </Box>
+  );
 };
 
 export default IntegrationFrame;
