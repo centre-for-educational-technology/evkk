1423d4071ceb652805db74a4f912ccf8031140ec
Harli Kodasma <harlikodasma@gmail.com>
2023-10-24 21:24:28 +0300

    ELLE-220 | fix formatting

--- ui/src/elle/pages/Tools.js
+++ ui/src/elle/pages/Tools.js
@@ -14,145 +14,145 @@ import WordAnalyser from '../resources/images/tools/sonaanalyys.png';
 import { TabStyle } from '../const/Constants';
 
 const ToolIconCard = (props) => {
-    return (
-        <>
-            <Box
-                className="tool-card-container"
-                sx={{boxShadow: 3}}>
-                <Box className="tool-card-icon">
-                    <img className="tool-icon-img" src={props.image} loading="lazy" alt="Tool logo"/>
-                </Box>
-                <Box className="tool-card-text">
-                    {props.t(props.text)}
-                </Box>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box
+        className="tool-card-container"
+        sx={{boxShadow: 3}}>
+        <Box className="tool-card-icon">
+          <img className="tool-icon-img" src={props.image} loading="lazy" alt="Tool logo"/>
+        </Box>
+        <Box className="tool-card-text">
+          {props.t(props.text)}
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 const TabOutlet = (props) => {
-    return (
-        <TabPanel value={props.value}>
-            {props.textsSelected ? <Outlet/> :
-                <Box>
-                    <ToolIconCard image={props.image} t={props.t} text={props.text}/>
-                    <Alert severity="warning">
-                        {props.t('tools_warning_text')}
-                    </Alert>
-                </Box>}
-        </TabPanel>
-    );
+  return (
+    <TabPanel value={props.value}>
+      {props.textsSelected ? <Outlet/> :
+        <Box>
+          <ToolIconCard image={props.image} t={props.t} text={props.text}/>
+          <Alert severity="warning">
+            {props.t('tools_warning_text')}
+          </Alert>
+        </Box>}
+    </TabPanel>
+  );
 };
 
 export default function Tools() {
-    const current = useLocation();
-    const queryOpen = current.state && current.state.pageNo === 'queryOpen' ? current.state.pageNo : null;
-    const {t} = useTranslation();
-    const navigate = useNavigate();
-    const [tabPage, setTabPage] = useState(current.pathname);
-    const [textsSelected, setTextsSelected] = useState(false);
+  const current = useLocation();
+  const queryOpen = current.state && current.state.pageNo === 'queryOpen' ? current.state.pageNo : null;
+  const {t} = useTranslation();
+  const navigate = useNavigate();
+  const [tabPage, setTabPage] = useState(current.pathname);
+  const [textsSelected, setTextsSelected] = useState(false);
 
-    useEffect(() => {
-        setTabPage(current.pathname);
-        if (current.pathname === '/tools') {
-            navigate('wordlist', {replace: true});
-        }
-    }, [current.pathname]);
+  useEffect(() => {
+    setTabPage(current.pathname);
+    if (current.pathname === '/tools') {
+      navigate('wordlist', {replace: true});
+    }
+  }, [current.pathname]);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const handleChange = (event, newValue) => {
-        setTabPage(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setTabPage(newValue);
+  };
 
-    queryStore.subscribe(() => {
-        const storeState = queryStore.getState();
-        setTextsSelected(storeState.corpusTextIds !== null || storeState.ownTexts !== null);
-    });
+  queryStore.subscribe(() => {
+    const storeState = queryStore.getState();
+    setTextsSelected(storeState.corpusTextIds !== null || storeState.ownTexts !== null);
+  });
 
-    console.log(textsSelected);
+  console.log(textsSelected);
 
-    return (
-        <Box className="outer-container-tools">
-            <Box className="tool-page-container-outer">
-                <Box className="outer-outer">
-                    <Query queryOpen={queryOpen}/>
-                </Box>
-                <Box className="tools-box-right">
-                    <TabContext value={tabPage} className="tab-context-class">
-                        <Box boxShadow={3} borderRadius={'25px'}>
-                            <TabList
-                                onChange={handleChange}
-                                TabIndicatorProps={{sx: {display: 'none'}}}
-                                sx={TabStyle}
-                            >
-                                <Tab
-                                    label={t('wordlist_filename')}
-                                    onClick={() => navigate('/tools/wordlist')}
-                                    value="/tools/wordlist"
-                                />
-                                <Tab
-                                    label={t('common_word_in_context')}
-                                    onClick={() => navigate('/tools/wordcontext')}
-                                    value="/tools/wordcontext"
-                                />
-                                <Tab
-                                    label={t('common_neighbouring_words')}
-                                    onClick={() => navigate('/tools/collocates')}
-                                    value="/tools/collocates"
-                                />
-                                <Tab
-                                    label={t('common_word_analyser')}
-                                    onClick={() => navigate('/tools/wordanalyser')}
-                                    value="/tools/wordanalyser"
-                                />
-                                <Tab
-                                    label={t('common_clusters')}
-                                    onClick={() => navigate('/tools/clusterfinder')}
-                                    value="/tools/clusterfinder"
-                                />
-                            </TabList>
-                        </Box>
-                        <TabOutlet
-                            t={t}
-                            textsSelected={textsSelected}
-                            image={WordlistImg}
-                            text={'tools_accordion_wordlist_explainer'}
-                            value="/tools/wordlist"
-                        />
-                        <TabOutlet
-                            t={t}
-                            textsSelected={textsSelected}
-                            image={WordContext}
-                            text={'tools_accordion_word_in_context_explainer'}
-                            value="/tools/wordcontext"
-                        />
-                        <TabOutlet
-                            t={t}
-                            textsSelected={textsSelected}
-                            image={NeighbourWord}
-                            text={'tools_accordion_neighbouring_words_explainer'}
-                            value="/tools/collocates"
-                        />
-                        <TabOutlet
-                            t={t}
-                            textsSelected={textsSelected}
-                            image={WordAnalyser}
-                            text={'tools_accordion_word_analysis_explainer'}
-                            value="/tools/wordanalyser"
-                        />
-                        <TabOutlet
-                            t={t}
-                            textsSelected={textsSelected}
-                            image={WordPattern}
-                            text={'tools_accordion_clusters_explainer'}
-                            value="/tools/clusterfinder"
-                        />
-                    </TabContext>
-                </Box>
-            </Box>
+  return (
+    <Box className="outer-container-tools">
+      <Box className="tool-page-container-outer">
+        <Box className="outer-outer">
+          <Query queryOpen={queryOpen}/>
         </Box>
-    );
+        <Box className="tools-box-right">
+          <TabContext value={tabPage} className="tab-context-class">
+            <Box boxShadow={3} borderRadius={'25px'}>
+              <TabList
+                onChange={handleChange}
+                TabIndicatorProps={{sx: {display: 'none'}}}
+                sx={TabStyle}
+              >
+                <Tab
+                  label={t('wordlist_filename')}
+                  onClick={() => navigate('/tools/wordlist')}
+                  value="/tools/wordlist"
+                />
+                <Tab
+                  label={t('common_word_in_context')}
+                  onClick={() => navigate('/tools/wordcontext')}
+                  value="/tools/wordcontext"
+                />
+                <Tab
+                  label={t('common_neighbouring_words')}
+                  onClick={() => navigate('/tools/collocates')}
+                  value="/tools/collocates"
+                />
+                <Tab
+                  label={t('common_word_analyser')}
+                  onClick={() => navigate('/tools/wordanalyser')}
+                  value="/tools/wordanalyser"
+                />
+                <Tab
+                  label={t('common_clusters')}
+                  onClick={() => navigate('/tools/clusterfinder')}
+                  value="/tools/clusterfinder"
+                />
+              </TabList>
+            </Box>
+            <TabOutlet
+              t={t}
+              textsSelected={textsSelected}
+              image={WordlistImg}
+              text={'tools_accordion_wordlist_explainer'}
+              value="/tools/wordlist"
+            />
+            <TabOutlet
+              t={t}
+              textsSelected={textsSelected}
+              image={WordContext}
+              text={'tools_accordion_word_in_context_explainer'}
+              value="/tools/wordcontext"
+            />
+            <TabOutlet
+              t={t}
+              textsSelected={textsSelected}
+              image={NeighbourWord}
+              text={'tools_accordion_neighbouring_words_explainer'}
+              value="/tools/collocates"
+            />
+            <TabOutlet
+              t={t}
+              textsSelected={textsSelected}
+              image={WordAnalyser}
+              text={'tools_accordion_word_analysis_explainer'}
+              value="/tools/wordanalyser"
+            />
+            <TabOutlet
+              t={t}
+              textsSelected={textsSelected}
+              image={WordPattern}
+              text={'tools_accordion_clusters_explainer'}
+              value="/tools/clusterfinder"
+            />
+          </TabContext>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
