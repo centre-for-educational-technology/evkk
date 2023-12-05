0a5b1823d202ac14dca4b9d81eab3c24e5d741a9
Harli Kodasma <harlikodasma@gmail.com>
2023-12-05 20:54:19 +0200

    ELLE-267 | code quality improvements

--- ui/src/elle/components/about/ContactCard.js
+++ ui/src/elle/components/about/ContactCard.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import './styles/ContactCard.css';
 import { ReactComponent as EtisLogo } from '../../resources/images/misc/etis_logo.svg';
-
-;
 
 export default function ContactCard({name, role, email, etisUrl, icon}) {
   return (
     <Box className="contact-box">
       <Box className="icon-box">
-        {icon
-          ? <img className="contact-image" src={icon}
-                 alt={icon}/>
-          : <AccountBoxIcon className="icon"/>
-        }
+        <img className="contact-image"
+             src={icon}
+             alt={icon}/>
       </Box>
       <Box className="text-box">
         <Box className="name-box">{name}</Box>
