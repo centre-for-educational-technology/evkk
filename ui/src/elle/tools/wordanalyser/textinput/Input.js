8e675eedf23556bda1dc5a2fb0e5840556382bfc
Harli Kodasma <harlikodasma@gmail.com>
2024-01-15 21:18:28 +0200

    ELLE-234 | remove & refactor unused code, fix bug with changing input text after analysis

--- ui/src/elle/tools/wordanalyser/textinput/Input.js
+++ ui/src/elle/tools/wordanalyser/textinput/Input.js
@@ -1,18 +1,13 @@
 import { InputText } from './InputText';
 import { useContext, useEffect, useState } from 'react';
-import { Alert, Button, CircularProgress, Grid } from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
+import { Alert, CircularProgress, Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import '../../../translations/i18n';
 import { AnalyseContext, TabContext } from '../Contexts';
-import { DefaultButtonStyle, DefaultCircularProgressStyle } from '../../../const/Constants';
+import { DefaultCircularProgressStyle } from '../../../const/Constants';
 
-export const Input = ({onInsert, onMarkWords, onWordSelect, onWordInfo, onReset, textFromFile}) => {
-  const [input, setInput] = useState(textFromFile);
+export const Input = ({onInsert, onMarkWords, onWordSelect, onWordInfo, onReset, inputText}) => {
   const [selectedWords, setSelectedWords] = useState(onMarkWords);
-  const [showAnalyseBtn, setShowAnalyseBtn] = useState(true);
-  const [showResetBtn, setShowResetBtn] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
   const [showLoading, setShowLoading] = useState(false);
   const [textTooLong, setTextTooLong] = useState(false);
   const analyse = useContext(AnalyseContext)[0];
@@ -21,23 +16,21 @@ export const Input = ({onInsert, onMarkWords, onWordSelect, onWordInfo, onReset,
 
   const onSubmit = () => {
     setTextTooLong(false);
-    setShowAlert(false);
-    if (textFromFile.replaceAll(/[^a-zA-ZõäüöÕÄÖÜ0-9]/g, ' ').replaceAll(/\s+/g, ' ').split(' ').length > 1020) {
+    if (inputText.replaceAll(/[^a-zA-ZõäüöÕÄÖÜ0-9]/g, ' ').replaceAll(/\s+/g, ' ').split(' ').length > 1020) {
       setTextTooLong(true);
-    } else if (textFromFile.length > 0) {
-      setShowAnalyseBtn(false);
-      onInsert(textFromFile);
+    } else if (inputText.length > 0) {
       setShowLoading(true);
-    } else {
-      setShowAlert(true);
+      onInsert(inputText);
     }
   };
 
   useEffect(() => {
-    if (textFromFile) {
+    if (inputText) {
+      resetAnalyser();
       onSubmit();
     }
-  }, [textFromFile]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputText]);
 
   useEffect(() => {
     if (JSON.stringify(selectedWords) !== JSON.stringify(onMarkWords)) {
@@ -46,59 +39,22 @@ export const Input = ({onInsert, onMarkWords, onWordSelect, onWordInfo, onReset,
   }, [onMarkWords, selectedWords]);
 
   useEffect(() => {
-    setShowLoading(false);
-    if (analyse.ids[0].length > 0) {
-      setShowResetBtn(true);
-    }
+    setShowLoading(analyse.text.length === 0);
   }, [analyse]);
 
   const resetAnalyser = () => {
     setTableValue(0);
-    setShowAnalyseBtn(true);
-    setShowResetBtn(false);
-    setInput('');
     onReset();
   };
 
-  useEffect(() => {
-    setInput(textFromFile);
-  }, [textFromFile]);
-
-  //Tekst on analüüsi kuvamiseks liiga pikk kui on üle 1000 sõna
-
   return (
     <div className="containerItem">
-      {/*<form hidden>
-          <label className="textInputContainer">
-            <textarea spellCheck="false"
-                      className="textInput"
-                      name="textInput"
-                      value={input}
-                      onChange={(e) => setInput(e.target.value)}/>
-          </label>
-          <Button sx={DefaultButtonStyle}
-                  variant="contained"
-                  onClick={onSubmit}>{t('analyse_button')}</Button>
-          {showAlert &&
-            <span>
-              <br/><br/>
-              <Alert severity="warning">{t('error_no_text')}</Alert>
-            </span>
-          }
-          {textTooLong &&
-            <Grid item
-                  xs={12}
-                  md={12}>
-              <br/>
-              <Alert severity="warning">{t('error_text_too_long')}</Alert>
-            </Grid>
-          }
-        </form>*/}
-      {showLoading && !textTooLong ?
+      {showLoading && !textTooLong &&
         <div
           className="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center">
-          <CircularProgress style={DefaultCircularProgressStyle} size={'5rem'}/></div>
-        :
+          <CircularProgress style={DefaultCircularProgressStyle} size={'5rem'} /></div>
+      }
+      {!textTooLong && !showLoading &&
         <InputText
           onMarkWords={onMarkWords}
           onWordSelect={onWordSelect}
@@ -109,17 +65,10 @@ export const Input = ({onInsert, onMarkWords, onWordSelect, onWordInfo, onReset,
         <Grid item
               xs={12}
               md={12}>
-          <br/>
+          <br />
           <Alert severity="warning">{t('error_text_too_long')}</Alert>
         </Grid>
       }
-      {showLoading && <LoadingButton hidden sx={DefaultButtonStyle}
-                                     loading
-                                     variant="outlined">{t('analyse_button')}</LoadingButton>}
-      {showResetBtn && <Button hidden sx={DefaultButtonStyle}
-                               variant="contained"
-                               className="mainBtn"
-                               onClick={resetAnalyser}>{t('reset_button')}</Button>}
     </div>
   );
 };
