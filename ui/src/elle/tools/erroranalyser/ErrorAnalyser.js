f45c80c23e1ad5eb446d9401bcc048a91374f21d
Martin Rünk <knyrr@tlu.ee>
2024-02-06 09:59:54 +0200

    Add Error Analyser checkboxes

--- ui/src/elle/tools/erroranalyser/ErrorAnalyser.js
+++ ui/src/elle/tools/erroranalyser/ErrorAnalyser.js
@@ -1,21 +1,29 @@
 import { useState, useEffect } from 'react';
+import {languageErrors, languageLevels} from "./ErrorEnums";
+import MultiLevelSelect from "./MultiLevelSelect";
+
+//TODO translation
+//TODO layout
 
 function ErrorAnalyser() {
   const [errorDetails, setErrorDetails] = useState(null);
 
-  useEffect(() => {
+  async function getErrors(){
     fetch('http://localhost:9090/api/errors/getErrors?error=LEX&level=B1')
       .then(response => response.json())
       .then(data => setErrorDetails(data))
       .catch(error => console.error('Error:', error));
-
     console.log(errorDetails)
+  }
+  useEffect(() => {
+    //
   }, []);
 
   return (
-    <div>
-      {errorDetails ? <pre>{JSON.stringify(errorDetails, null, 2)}</pre> : 'Loading...'}
-    </div>
+  <div>
+    {/*{errorDetails ? <pre>{JSON.stringify(errorDetails, null, 2)}</pre> : 'Loading...'}*/}
+    <MultiLevelSelect/>
+  </div>
   );
 }
 
