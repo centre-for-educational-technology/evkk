ae79273a677e945faa6bd0735968c8d24eef4a87
Martin Rünk <martin.rynk@gmail.com>
2024-02-16 11:33:48 +0200

    Update Error Analyser getFilterEnums

--- dal/src/main/java/ee/tlu/evkk/dal/dto/ErrorAnalyserEnums.java
+++ dal/src/main/java/ee/tlu/evkk/dal/dto/ErrorAnalyserEnums.java
@@ -5,14 +5,22 @@ import java.util.List;
 public class ErrorAnalyserEnums {
     private List<String> languageLevels;
     private List<String> nativeLanguages;
+    private List<String> citizenship;
+    private List<String> textType;
+    private List<String> education;
+    private List<String> age;
 
-    public ErrorAnalyserEnums(List<String> languageLevels, List<String> nativeLanguage) {
+    public ErrorAnalyserEnums(List<String> languageLevels, List<String> nativeLanguage, List<String> citizenship,
+            List<String> textType, List<String> education, List<String> age) {
         this.languageLevels = languageLevels;
         this.nativeLanguages = nativeLanguage;
+        this.citizenship = citizenship;
+        this.textType = textType;
+        this.education = education;
+        this.age = age;
     }
 
     public ErrorAnalyserEnums() {
-
     }
 
     public List<String> getLanguageLevels() {
@@ -21,5 +29,21 @@ public class ErrorAnalyserEnums {
 
     public List<String> getNativeLanguages() {
         return nativeLanguages;
+    }
+
+    public List<String> getCitizenship() {
+        return citizenship;
+    }
+
+    public List<String> getTextType() {
+        return textType;
+    }
+
+    public List<String> getEducation() {
+        return education;
+    }
+
+    public List<String> getAge() {
+        return age;
     }
 }
