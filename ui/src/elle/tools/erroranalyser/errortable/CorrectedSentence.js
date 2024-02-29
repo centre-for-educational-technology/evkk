07f4839fd28ad85084753756647b542fe2b95df3
Martin Rünk <martin.rynk@gmail.com>
2024-02-29 09:25:35 +0200

    Update annotation rendering

--- ui/src/elle/tools/erroranalyser/errortable/CorrectedSentence.js
+++ ui/src/elle/tools/erroranalyser/errortable/CorrectedSentence.js
@@ -1,89 +1,13 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import { Box } from '@mui/material';
 import './../ErrorAnalyser.css';
 import AnnotatedWord from './AnnotatedWord';
 
-export default function CorrectedSentence({ annotations, sentence }) {
-  const [sortedSentence, setSortedSentence] = useState();
-  const applyAnnotations = (annotations, sentence) => {
-    const modifiedSentence = structuredClone(sentence);
-    annotations.forEach((annotation, key) => {
-      const errorType = annotation.errorType;
-
-      switch (errorType[0]) {
-        case 'M': //missing => added
-          const addedItem = {
-            ...annotation,
-            status: 'added',
-          };
-          modifiedSentence.set(key, addedItem);
-          break;
-        case 'U': //unnecessary => deleted
-          const deletedItemKey = [
-            annotation.scopeStart,
-            annotation.scopeEnd,
-            -1,
-          ].join('::');
-
-          const sourceDeletedItem = sentence.get(deletedItemKey);
-          if (sourceDeletedItem) {
-            const deletedItem = {
-              ...sourceDeletedItem,
-              status: 'deleted',
-            };
-            modifiedSentence.set(deletedItemKey, deletedItem);
-          }
-          break;
-        default: //"R" => replaced
-          for (let i = annotation.scopeStart; i < annotation.scopeEnd; i++) {
-            const replacedItemKey = [i, i + 1, -1].join('::');
-            const sourceReplacedItem = sentence.get(replacedItemKey);
-            if (sourceReplacedItem) {
-              const replacedDeletedItem = {
-                ...sourceReplacedItem,
-                status: 'replaced-deleted',
-              };
-              modifiedSentence.set(replacedItemKey, replacedDeletedItem);
-            }
-          }
-          const replacedAddedItem = {
-            ...annotation,
-            status: 'replaced-added',
-          };
-          modifiedSentence.set(key, replacedAddedItem);
-      }
-    });
-    return modifiedSentence;
-  };
-
-  const sortSentence = (sentence) => {
-    const sortedSentences = Array.from(sentence.entries())
-      .map(([key, value]) => {
-        const intKey = key.split('::').map(Number);
-        return [intKey, value];
-      })
-      .sort((a, b) => {
-        for (let i = 0; i < 3; i++) {
-          if (a[0][i] !== b[0][i]) {
-            return a[0][i] - b[0][i];
-          }
-        }
-        return 0;
-      })
-      .map((entry) => entry[1]);
-
-    return sortedSentences;
-  };
-
-  useEffect(() => {
-    const modifiedSentence = applyAnnotations(annotations, sentence);
-    setSortedSentence(sortSentence(modifiedSentence));
-  }, [annotations, sentence]);
-
+export default function CorrectedSentence({ sentence }) {
   return (
     <Box className="corrected-sentence">
-      {sortedSentence &&
-        sortedSentence.map((item, index) => {
+      {sentence &&
+        sentence.map((item, index) => {
           if (item.status !== 'deleted' && item.status !== 'replaced-deleted') {
             if (item.status === 'initial') {
               return (
