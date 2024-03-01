9944e5c4561641df2a7c019cc950f0ecfadd52c1
Martin Rünk <martin.rynk@gmail.com>
2024-03-01 12:20:35 +0200

    Group annotations in BE

--- dal/src/main/java/ee/tlu/evkk/dal/dto/ErrorAnalyserSentence.java
+++ dal/src/main/java/ee/tlu/evkk/dal/dto/ErrorAnalyserSentence.java
@@ -1,6 +1,8 @@
 package ee.tlu.evkk.dal.dto;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +19,8 @@ public class ErrorAnalyserSentence {
   private String education;
   private String citizenship;
   private List<ErrorAnalyserAnnotation> annotations;
-  private Map<String, Integer> errorTypes;
+  private List<List<ErrorAnalyserAnnotation>> groupedAnnotations;
+  private Map<Integer, Map<String, Integer>> errorTypes;
 
   public ErrorAnalyserSentence(String sentenceId, String sentence, String textId, String languageLevel,
       String nativeLanguage, String textType, String age, String ageRange, String education, String citizenship,
@@ -78,46 +81,60 @@ public class ErrorAnalyserSentence {
     return citizenship;
   }
 
-  public List<ErrorAnalyserAnnotation> getAnnotations() {
-    return annotations;
-  }
+  // public List<ErrorAnalyserAnnotation> getAnnotations() {
+  // return annotations;
+  // }
 
-  public Map<String, Integer> getErrorTypes() {
-    Map<String, Integer> errorCounter = countErrors(annotations);
-    return errorCounter;
-  }
-
-  public static Map<String, Integer> countErrors(List<ErrorAnalyserAnnotation> annotations) {
-    Map<String, Integer> errors = new HashMap<>();
+  public List<List<ErrorAnalyserAnnotation>> getGroupedAnnotations() {
+    List<List<ErrorAnalyserAnnotation>> annotationGroups = new ArrayList<>();
     for (ErrorAnalyserAnnotation annotation : annotations) {
-      String errorType = annotation.getErrorType();
+      Integer annotatorId = Integer.valueOf(annotation.getAnnotatorId());
+      if (annotatorId >= annotationGroups.size()) {
+        // Expand the list to accommodate the annotatorId
+        annotationGroups.addAll(Collections.nCopies(annotatorId - annotationGroups.size() + 1, null));
+      }
+      // Ensure there is a list for the annotatorId
+      if (annotationGroups.get(annotatorId) == null) {
+        annotationGroups.set(annotatorId, new ArrayList<>());
+      }
+      List<ErrorAnalyserAnnotation> annotationGroup = annotationGroups.get(annotatorId);
+      annotationGroup.add(annotation);
+    }
+    return annotationGroups;
+  }
 
+  public Map<Integer, Map<String, Integer>> getErrorTypes() {
+
+    Map<Integer, Map<String, Integer>> errorLists = new HashMap<>();
+
+    for (ErrorAnalyserAnnotation annotation : annotations) {
+      Integer annotatorId = Integer.valueOf(annotation.getAnnotatorId());
+      String errorType = annotation.getErrorType();
       List<String> mainErrorTypes = Arrays.asList("U:LEX", "U:PUNCT", "M:LEX", "M:PUNCT", "R:LEX", "R:CASE",
           "R:NOM:FORM", "R:PUNCT", "R:SPELL", "R:VERB:FORM", "R:WO", "R:WS");
-
       List<String> compoundErrorTypesStart = Arrays.asList("R:LEX", "R:NOM:FORM", "R:SPELL", "R:VERB:FORM", "R:WO",
           "R:WS");
-
       List<String> compoundErrorTypesEnd = Arrays.asList("CASE", "NOM:FORM", "SPELL", "VERB:FORM", "WO", "WS");
 
+      Map<String, Integer> errorList = errorLists.getOrDefault(annotatorId, new HashMap<>());
       if (mainErrorTypes.contains(errorType)) {
-        errors.put(errorType, errors.getOrDefault(errorType, 0) + 1);
+        errorList.put(errorType, errorList.getOrDefault(errorType, 0) + 1);
       } else {
         for (String firstError : compoundErrorTypesStart) {
           if (errorType.startsWith(firstError)) {
-            errors.put(firstError, errors.getOrDefault(firstError, 0) + 1);
+            errorList.put(firstError, errorList.getOrDefault(errorType, 0) + 1);
             String remaining = errorType.substring(firstError.length() + 1);
             if (!remaining.isEmpty()) {
               for (String secondError : compoundErrorTypesEnd) {
                 if (remaining.startsWith(secondError)) {
                   String modifiedSecondError = "R:" + secondError;
-                  errors.put(modifiedSecondError, errors.getOrDefault(modifiedSecondError, 0) + 1);
+                  errorList.put(modifiedSecondError, errorList.getOrDefault(modifiedSecondError, 0) + 1);
                   remaining = remaining.substring(secondError.length());
                   if (!remaining.isEmpty()) {
                     for (String thirdError : compoundErrorTypesEnd) {
                       if (remaining.startsWith(thirdError)) {
                         String modifiedThirdError = "R:" + thirdError;
-                        errors.put(modifiedThirdError, errors.getOrDefault(modifiedThirdError, 0) + 1);
+                        errorList.put(modifiedThirdError, errorList.getOrDefault(modifiedThirdError, 0) + 1);
                         break;
                       }
                     }
@@ -130,7 +147,8 @@ public class ErrorAnalyserSentence {
           }
         }
       }
+      errorLists.put(annotatorId, errorList);
     }
-    return errors;
+    return errorLists;
   }
 }
