ffa725ff9afbb1196308c6f2a7f0d0d2875473af
Martin <martin.rynk@gmail.com>
2024-06-11 11:11:37 +0300

    Refactor code

--- dal/src/main/java/ee/tlu/evkk/dal/dto/ErrorAnalyserTransformedAnnotation.java
+++ dal/src/main/java/ee/tlu/evkk/dal/dto/ErrorAnalyserTransformedAnnotation.java
@@ -3,51 +3,51 @@ package ee.tlu.evkk.dal.dto;
 import java.util.List;
 
 public class ErrorAnalyserTransformedAnnotation {
-    private final String annotationId;
-    private final String annotatorId;
-    private final String scopeStart;
-    private final String scopeEnd;
-    private final String errorType;
-    private final List<String> errorTypes;
-    private final String correction;
+  private final String annotationId;
+  private final String annotatorId;
+  private final String scopeStart;
+  private final String scopeEnd;
+  private final String errorType;
+  private final List<String> extractedErrorTypes;
+  private final String content;
 
-    public ErrorAnalyserTransformedAnnotation(String annotationId, String annotatorId, String scopeStart,
-            String scopeEnd, String errorType, List<String> errorTypes, String correction) {
-        this.annotationId = annotationId;
-        this.annotatorId = annotatorId;
-        this.scopeStart = scopeStart;
-        this.scopeEnd = scopeEnd;
-        this.errorType = errorType;
-        this.errorTypes = errorTypes;
-        this.correction = correction;
-    }
+  public ErrorAnalyserTransformedAnnotation(String annotationId, String annotatorId, String scopeStart,
+                                            String scopeEnd, String errorType, List<String> extractedErrorTypes, String correction) {
+    this.annotationId = annotationId;
+    this.annotatorId = annotatorId;
+    this.scopeStart = scopeStart;
+    this.scopeEnd = scopeEnd;
+    this.errorType = errorType;
+    this.extractedErrorTypes = extractedErrorTypes;
+    this.content = correction;
+  }
 
-    public String getAnnotationId() {
-        return annotationId;
-    }
+  public String getAnnotationId() {
+    return annotationId;
+  }
 
-    public String getAnnotatorId() {
-        return annotatorId;
-    }
+  public String getAnnotatorId() {
+    return annotatorId;
+  }
 
-    public String getScopeStart() {
-        return scopeStart;
-    }
+  public String getScopeStart() {
+    return scopeStart;
+  }
 
-    public String getScopeEnd() {
-        return scopeEnd;
-    }
+  public String getScopeEnd() {
+    return scopeEnd;
+  }
 
-    public String getErrorType() {
-        return errorType;
-    }
+  public String getErrorType() {
+    return errorType;
+  }
 
-    public List<String> getErrorTypes() {
-        return errorTypes;
-    }
+  public List<String> getExtractedErrorTypes() {
+    return extractedErrorTypes;
+  }
 
-    public String getCorrection() {
-        return correction;
-    }
+  public String getContent() {
+    return content;
+  }
 
 }
