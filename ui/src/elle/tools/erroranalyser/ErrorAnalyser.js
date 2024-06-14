2799f563b3d4e50f016f3c18cf2e15a7bdc324e1
Martin Rünk <martin.rynk@gmail.com>
2024-06-14 16:36:42 +0300

    Refactor code

--- ui/src/elle/tools/erroranalyser/ErrorAnalyser.js
+++ ui/src/elle/tools/erroranalyser/ErrorAnalyser.js
@@ -19,6 +19,7 @@ export default function ErrorAnalyser() {
   const [showAllErrors, setShowAllErrors] = useState(false);
   const { t } = useTranslation();
 
+
   const getData = (
     errorTypeFilter,
     languageLevelFilter,
