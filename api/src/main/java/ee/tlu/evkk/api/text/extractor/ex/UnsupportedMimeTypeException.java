feb4afda6473761981feeee05d6a2b301a985d4e
Harli Kodasma <harlikodasma@gmail.com>
2024-08-19 01:11:40 +0300

    ELLE-294 | simplify UnsupportedMimeTypeException

--- api/src/main/java/ee/tlu/evkk/api/text/extractor/ex/UnsupportedMimeTypeException.java
+++ api/src/main/java/ee/tlu/evkk/api/text/extractor/ex/UnsupportedMimeTypeException.java
@@ -2,18 +2,9 @@ package ee.tlu.evkk.api.text.extractor.ex;
 
 import ee.tlu.evkk.api.exception.AbstractBusinessException;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.util.MimeType;
 
-/**
- * @author Mikk Tarvas
- * Date: 04.06.2020
- */
 @Getter
-@RequiredArgsConstructor
 public class UnsupportedMimeTypeException extends AbstractBusinessException {
-
-  private final MimeType mimeType;
 
   @Override
   public String getCode() {
