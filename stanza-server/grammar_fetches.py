986a7defcb95165ba877022a99f52d9619f05eac
Harli Kodasma <harlikodasma@gmail.com>
2024-10-14 22:30:45 +0300

    ELLE-310 | simplify code

--- stanza-server/grammar_fetches.py
+++ stanza-server/grammar_fetches.py
@@ -3,18 +3,19 @@ import requests
 headers = {'Content-Type': 'application/json'}
 data_text = 'text'
 
+grammar_url = 'https://api.tartunlp.ai/grammar'
+grammar_alt_url = 'http://grammar-worker-server:5400/grammarchecker'
+speller_url = 'http://grammar-worker-server:5400/spellchecker'
+
 
 def fetch_grammar(text):
-    url = 'https://api.tartunlp.ai/grammar'
-    alt_url = 'http://grammar-worker-server:5400/grammarchecker'
     data = {data_text: text}
-
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(grammar_url, headers=headers, json=data)
 
     if response.status_code == 200:
         return response.json()
     else:
-        response = requests.post(alt_url, headers=headers, json=data)
+        response = requests.post(grammar_alt_url, headers=headers, json=data)
         if response.status_code == 200:
             return response.json()
         else:
@@ -22,10 +23,8 @@ def fetch_grammar(text):
 
 
 def fetch_speller(text):
-    url = 'http://grammar-worker-server:5400/spellchecker'
     data = {data_text: text}
-
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(speller_url, headers=headers, json=data)
 
     if response.status_code == 200:
         return response.json()
