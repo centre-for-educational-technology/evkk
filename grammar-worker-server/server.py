1899cd0b44a601e77eb100c1cfc346013ddffefd
Taavi Kamarik <90038572+TaaviKamarik@users.noreply.github.com>
2024-10-14 22:31:31 +0300

    ELLE-310 | Add NELB model (#328)

--- grammar-worker-server/server.py
+++ grammar-worker-server/server.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 speller_config = read_speller_config("models/spell_etnc19_reference_corpus_6000000_web_2019_600000.yaml")
 speller = Speller(speller_config)
 
-grammar_config = read_gec_config('models/GEC-synthetic-pretrain-ut-ft.yaml')
+grammar_config = read_gec_config('models/GEC-nelb-1.3b.yaml')
 grammar = GEC(grammar_config)
 
 methods = ['POST']
 language = 'et'
-json_request_text = 'tekst'
+json_request_text = 'text'
 
 
 @app.route('/spellchecker', methods=methods)
