e70e1f25d3538c96313f58790b1cfdb3c17fd8d7
Harli Kodasma <harlikodasma@gmail.com>
2024-11-04 22:56:26 +0200

    ELLE-295 | fix more data consistency in db

--- db/src/main/resources/migration/repeatable/R__0013-core.text_property_correction.sql
+++ db/src/main/resources/migration/repeatable/R__0013-core.text_property_correction.sql
@@ -9,13 +9,14 @@ where property_value = 'ak_uurimus_bakalaureusetoo';
 update core.text_property
 set property_value = 'k1eesti_harjutus'
 where text_id in (
-                  'af71d7da-6ae0-4048-b1fd-f2f83d8aee5a',
-                  'b0ca6f22-e126-46cf-a57c-7c2889cf1f2f',
-                  '3739d73c-d4d0-45ab-9cfa-6b6c03ea67e2',
-                  '344f5cb4-cde1-402a-8924-b5e28888340f',
-                  'df7b5411-4fcb-43da-ad9f-fdd2d2928ad7',
-                  '602c4e79-a1dd-468c-baab-6bda8cc53e5e',
-                  'd837c59a-af42-4546-827b-bb1393da1849'
+                  'af71d7da-6ae0-4048-b1fd-f2f83d8aee5a', 'b0ca6f22-e126-46cf-a57c-7c2889cf1f2f',
+                  '3739d73c-d4d0-45ab-9cfa-6b6c03ea67e2', '344f5cb4-cde1-402a-8924-b5e28888340f',
+                  'df7b5411-4fcb-43da-ad9f-fdd2d2928ad7', '602c4e79-a1dd-468c-baab-6bda8cc53e5e',
+                  'd837c59a-af42-4546-827b-bb1393da1849', '455515d3-f970-446d-9f1f-835370143397',
+                  'c11bc338-7d7e-421d-a47c-dcb749f822bc', 'c4bca182-87cb-4596-9ea6-7210ace49aa6',
+                  '3325c323-8b15-4b5e-a7b2-2ce312e88af7', 'd7bde7ed-77b1-4806-ace1-2b7bf2ff47dc',
+                  '71846d09-e4b0-4c96-9808-d31061f143a8', 'bad816a6-35ed-414f-a151-97062b9dea5e',
+                  '63d27ed6-0464-4a3f-aaf7-f1faad177141'
   )
   and property_name = 'tekstityyp';
 
@@ -39,13 +40,9 @@ where text_id in ('1084e8b3-09f1-4d7f-badb-0a3940889dc0', '1995ac4e-ae8c-4d4d-83
 
 update core.text_property
 set property_value = 'cYDRkpymb'
-where text_id = '1995ac4e-ae8c-4d4d-835a-995ca700d65a'
+where text_id in ('1995ac4e-ae8c-4d4d-835a-995ca700d65a', '455515d3-f970-446d-9f1f-835370143397',
+                  'c11bc338-7d7e-421d-a47c-dcb749f822bc', 'c4bca182-87cb-4596-9ea6-7210ace49aa6',
+                  '3325c323-8b15-4b5e-a7b2-2ce312e88af7', 'd7bde7ed-77b1-4806-ace1-2b7bf2ff47dc',
+                  '71846d09-e4b0-4c96-9808-d31061f143a8', 'bad816a6-35ed-414f-a151-97062b9dea5e',
+                  '63d27ed6-0464-4a3f-aaf7-f1faad177141')
   and property_name = 'korpus';
-
-update core.text_property
-set property_value = 'k1eesti_harjutus'
-where text_id in ('3739d73c-d4d0-45ab-9cfa-6b6c03ea67e2', '344f5cb4-cde1-402a-8924-b5e28888340f',
-                  'df7b5411-4fcb-43da-ad9f-fdd2d2928ad7', '602c4e79-a1dd-468c-baab-6bda8cc53e5e',
-                  'd837c59a-af42-4546-827b-bb1393da1849', 'b0ca6f22-e126-46cf-a57c-7c2889cf1f2f',
-                  'af71d7da-6ae0-4048-b1fd-f2f83d8aee5a')
-  and property_name = 'tekstityyp';
