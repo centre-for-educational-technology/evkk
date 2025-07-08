b6109691fead42bc87d8b8dfdd150c08fb6ad424
Harli Kodasma <harli.kodasma@askend.com>
2025-07-08 02:18:48 +0200

    ELLE-340 | add Windows build and run scripts, adjust README accordingly

---
+++ build-images.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = 'Stop'
+
+# Order is important here because images may depend on each other
+docker build . -f ./images/evkk-anaconda-2021-05.Dockerfile -t evkk-anaconda-2021-05
+docker build . -f ./images/evkk-estnltk141.Dockerfile -t evkk-estnltk141
+docker build . -f ./images/evkk-stanza.Dockerfile -t evkk-stanza
+docker build . -f ./images/evkk-jdk8-mvn.Dockerfile -t evkk-jdk8-mvn
